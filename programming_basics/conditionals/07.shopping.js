9f7b5d718e7b8f239fc537fd0b4097a4aadb7fcd
NikolayDelyanov <125092041+NikolayDelyanov@users.noreply.github.com>
2023-02-24 16:38:00 +0200

    07. Shopping

--- programming_basics/conditionals/07.shopping.js
+++ programming_basics/conditionals/07.shopping.js
@@ -7,24 +7,25 @@ let numOfRamMemory = Number(input[3]);
 
 let sumForVideocards = numOfVideocards * 250;
 let priceForProcessor = sumForVideocards * 0.35;
-let sumForProcessor = numOfProcessor * 175;
+let sumForProcessor = numOfProcessor * priceForProcessor;
 let priceForRam = sumForVideocards * 0.10;
-let sumForRam = numOfRamMemory *priceForRam;
+let sumForRam = numOfRamMemory * priceForRam;
 
 let totalSum = sumForVideocards + sumForProcessor + sumForRam;
 
 let discount = 0;
 
 if(numOfVideocards > numOfProcessor){
-    discount = totalSum - 0.15;
+     discount = totalSum * (15/100);
 }
+totalSum = totalSum - discount;
 
 let diff = Math.abs(totalSum - budget);
 
 if(discount <= budget){
-    console.log(`You have ${diff} leva left!`);
-}else if(totalSum > budget){
-    console.log(`Not enough money! You need ${diff} leva more!`);
+    console.log(`You have ${diff.toFixed(2)} leva left!`);
+}else if(totalSum >= budget){
+    console.log(`Not enough money! You need ${diff.toFixed(2)} leva more!`);
 }
 
 }
