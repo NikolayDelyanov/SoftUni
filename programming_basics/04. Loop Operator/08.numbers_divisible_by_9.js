3f489a56c66f5a83e3717918de857115161b6283
NikolayDelyanov <125092041+NikolayDelyanov@users.noreply.github.com>
2023-03-07 22:18:28 +0200

    08. Number divisible

---
+++ programming_basics/04. Loop Operator/08.numbers_divisible_by_9.js
@@ -0,0 +1,21 @@
+function numbersDivisible (input){
+
+let start = Number(input[0]);
+let end = Number(input[1]);
+
+let sum = 0;
+let numbers = "";
+
+for(let i = start; i <= end; i++){
+    if(i % 9 == 0){
+       
+        numbers += (i + " ");
+    }
+}
+
+console.log(`The sum: ${sum}`);
+console.log(numbers.trim());
+
+
+}
+numbersDivisible(["100", "200"]);
