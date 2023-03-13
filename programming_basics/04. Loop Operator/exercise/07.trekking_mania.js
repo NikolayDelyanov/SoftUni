47cd1021e8a6a7fc4069829fa961372c1713469b
NikolayDelyanov <125092041+NikolayDelyanov@users.noreply.github.com>
2023-03-14 00:34:42 +0200

    07. Trekking Mania

--- programming_basics/04. Loop Operator/exercise/07.trekking_mania.js
+++ programming_basics/04. Loop Operator/exercise/07.trekking_mania.js
@@ -1,48 +1,54 @@
-function trekkingMania(input){
+function trekkingMania(input) {
 
-let numOfGroups = Number(input[0]);
-let numOfPeople = Number(input[1]);
+    let numOfGroups = Number(input[0]);
 
-let top = 0;
-let procent = 0;
+    let totalNumOfPeople = 0;
 
-for(let i = 1; i <= input.length; i++){
-    
-    let totalNumOfPeople = input[i];
-    
-    if(numOfPeople < 6){
-        top = 'Musala';
-        procent = procent *(totalNumOfPeople / 100);
-    }else if(numOfPeople <= 12){
-        top = 'Monblan';
-        procent = procent *(totalNumOfPeople / 100);
-    }else if(numOfPeople <= 25){
-        top = 'Kilimandjaro';
-        procent = procent *(totalNumOfPeople / 100);
-    }else if(numOfPeople <= 40){
-        top = 'K2';
-        procent = procent *(totalNumOfPeople / 100);
-    }else if(numOfPeople >= 41){
-        top = 'Everest';
-        procent = procent *(totalNumOfPeople / 100);
+    let musalaPeople = 0;
+    let monblanPeople = 0;
+    let kilimandjaroPeople = 0;
+    let k2People = 0;
+    let everestPeople = 0;
+
+    for (let i = 1; i < input.length; i++) {
+
+        let numOfPeople = Number(input[i]);
+        totalNumOfPeople = totalNumOfPeople + numOfPeople;
+
+        if (numOfPeople < 6) {
+            musalaPeople += numOfPeople;
+        } else if (numOfPeople <= 12) {
+            monblanPeople += numOfPeople;
+        } else if (numOfPeople <= 25) {
+            kilimandjaroPeople += numOfPeople;
+        } else if (numOfPeople <= 40) {
+            k2People += numOfPeople;
+        } else if (numOfPeople >= 41) {
+            everestPeople += numOfPeople;
+        }
     }
-}
 
-console.log(`${input[i]} ${procent.toFixed(2)}`);
-console.log(`${input[i]} ${procent.toFixed(2)}`);
-console.log(`${input[i]} ${procent.toFixed(2)}`);
-console.log(`${input[i]} ${procent.toFixed(2)}`);
-console.log(`${input[i]} ${procent.toFixed(2)}`);
+    musalaPeople = (musalaPeople / totalNumOfPeople) * 100;
+    monblanPeople = (monblanPeople / totalNumOfPeople) * 100;
+    kilimandjaroPeople = (kilimandjaroPeople / totalNumOfPeople) * 100;
+    k2People = (k2People / totalNumOfPeople) * 100;
+    everestPeople = (everestPeople / totalNumOfPeople) * 100;
+
+    console.log(`${musalaPeople.toFixed(2)}%`);
+    console.log(`${monblanPeople.toFixed(2)}%`);
+    console.log(`${kilimandjaroPeople.toFixed(2)}%`);
+    console.log(`${k2People.toFixed(2)}%`);
+    console.log(`${everestPeople.toFixed(2)}%`);
 
 }
 trekkingMania(["10",
-"10",
-"5",
-"1",
-"100",
-"12",
-"26", 
-"17",
-"37",
-"40",
-"78"]);
+    "10",
+    "5",
+    "1",
+    "100",
+    "12",
+    "26",
+    "17",
+    "37",
+    "40",
+    "78"]);
