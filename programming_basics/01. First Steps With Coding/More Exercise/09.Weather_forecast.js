4b398c6c7798d5edde95e6a9093f83a7b9fc156b
NikolayDelyanov <125092041+NikolayDelyanov@users.noreply.github.com>
2023-03-17 17:55:22 +0200

    09. Weather forecast

---
+++ programming_basics/01. First Steps With Coding/More Exercise/09.Weather_forecast.js
@@ -0,0 +1,13 @@
+function weatherForecast(input) {
+
+    let text = input[0];
+
+    if (text == "sunny") {
+        console.log("It's warm outside!");
+    } else {
+        console.log("It's cold outside!");
+    }
+
+
+}
+weatherForecast(["sunny"]);
