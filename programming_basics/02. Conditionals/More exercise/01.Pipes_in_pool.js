c9a752a9e647a1e56aef2d97f48417dd7fd6f2e7
NikolayDelyanov <125092041+NikolayDelyanov@users.noreply.github.com>
2023-03-19 00:15:16 +0200

    01. Pipes in POOL

--- programming_basics/02. Conditionals/More exercise/01.Pipes_in_pool.js
+++ programming_basics/02. Conditionals/More exercise/01.Pipes_in_pool.js
@@ -1,29 +1,29 @@
-function pipesInPool(input){
+function pipesInPool(input) {
 
-let v = Number(input[0]);
-let pipe1 = Number(input[1]);
-let pipe2 = Number(input[2]);
-let hours = Number(input[3]);
+    let v = Number(input[0]);
+    let pipe1 = Number(input[1]);
+    let pipe2 = Number(input[2]);
+    let hours = Number(input[3]);
 
-let pipe1Volume = pipe1 * hours;
-let pipe2Volume = pipe2 * hours;
+    let pipe1Volume = pipe1 * hours;
+    let pipe2Volume = pipe2 * hours;
 
-let totalVolume = (pipe1 + pipe2) * hours;
-let remainingVolume =  v - totalVolume;
+    let totalVolume = (pipe1 + pipe2) * hours;
+    let remainingVolume = v - totalVolume;
 
-let procentPipe1 = (pipe1Volume / totalVolume) * 100;
-let procentPipe2 = (pipe2Volume / totalVolume) * 100;
-let poolOccupancy = (totalVolume / v) * 100;
+    let procentPipe1 = (pipe1Volume / totalVolume) * 100;
+    let procentPipe2 = (pipe2Volume / totalVolume) * 100;
+    let poolOccupancy = (totalVolume / v) * 100;
 
-console.log(`The pool is ${poolOccupancy.toFixed(2)}% full. Pipe 1: ${procentPipe1.toFixed(2)}%. Pipe 2: ${procentPipe2.toFixed(2)}%.`);
+    if (totalVolume > v) {
+        v = (v - remainingVolume) / hours;
+        console.log(`For ${hours.toFixed(2)} hours the pool overflows with ${Math.abs(remainingVolume).toFixed(2)} liters.`);
+    } else {
+        console.log(`The pool is ${poolOccupancy.toFixed(2)}% full. Pipe 1: ${procentPipe1.toFixed(2)}%. Pipe 2: ${procentPipe2.toFixed(2)}%.`);
+    }
 
-
-if(remainingVolume > v){
-    console.log(`For ${hours.toFixed(2)} hours the pool overflows with ${totalVolume.toFixed(2)} liters.`);
 }
-
-}
-pipesInPool(["1000",
-"100",
-"120",
-"3"]);
+pipesInPool(["100",
+    "100",
+    "100",
+    "2.5"]);
