3d4c53ea8596b52483a0b838512d0ac2bdb0f983
NikolayDelyanov <125092041+NikolayDelyanov@users.noreply.github.com>
2023-03-21 17:57:50 +0200

    02. Sleepy Tom cat

--- programming_basics/02. Conditionals/More exercise/02.Sleepy_tom_cat.js
+++ programming_basics/02. Conditionals/More exercise/02.Sleepy_tom_cat.js
@@ -1,31 +1,31 @@
-function sleepyTomCat(input){
+function sleepyTomCat(input) {
 
-let numberOfdaysOff = Number(input[0]);
+    let numberOfdaysOff = Number(input[0]);
 
-let playTimeInYear = 30000;
-let oneYear = 365;
+    let playTimeInYear = 30000;
+    let oneYear = 365;
 
-let holidayDayPlay = numberOfdaysOff * 127;
-let workDay = oneYear - numberOfdaysOff;
-let workDayPlay = workDay * 63;
+    let holidayDayPlay = numberOfdaysOff * 127;
+    let workDay = oneYear - numberOfdaysOff;
+    let workDayPlay = workDay * 63;
 
-let totalTimePlay = workDayPlay + holidayDayPlay;
-let difference = totalTimePlay - playTimeInYear;
+    let totalTimePlay = workDayPlay + holidayDayPlay;
+    let difference = totalTimePlay - playTimeInYear;
 
-let hours = 0;
-let minutes = 0;
+    let hours = 0;
+    let minutes = 0;
 
-if(playTimeInYear < totalTimePlay){
-     hours = (Math.floor(difference / 60).toFixed(0));
-     minutes = Math.floor(difference - (60 * hours)); 
-    console.log("Tom will run away");
-    console.log(`${Math.abs(hours)} hours and ${Math.abs(minutes)} minutes more for play`);
-}else if(playTimeInYear > totalTimePlay){
-     hours = ((difference / 60).toFixed(0));
-     minutes = (difference - (60 * hours));
-    console.log("Tom sleeps well");
-    console.log(`${Math.abs(hours)} hours and ${Math.abs(minutes)} minutes less for play`);
-}
+    if (playTimeInYear < totalTimePlay) {
+        hours = (Math.floor(difference / 60).toFixed(0));
+        minutes = Math.floor(difference - (60 * hours));
+        console.log("Tom will run away");
+        console.log(`${Math.abs(hours)} hours and ${Math.abs(minutes)} minutes more for play`);
+    } else if (playTimeInYear > totalTimePlay) {
+        hours = ((difference / 60).toFixed(0));
+        minutes = (difference - (60 * hours));
+        console.log("Tom sleeps well");
+        console.log(`${Math.abs(hours)} hours and ${Math.abs(minutes)} minutes less for play`);
+    }
 
 }
 sleepyTomCat(["75"]);
