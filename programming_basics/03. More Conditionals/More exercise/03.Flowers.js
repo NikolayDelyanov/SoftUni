12089e3ffc86b89df02f7d122d0fa0e346f1a661
NikolayDelyanov <125092041+NikolayDelyanov@users.noreply.github.com>
2023-03-25 14:08:14 +0200

    03. Flowers

--- programming_basics/03. More Conditionals/More exercise/03.Flowers.js
+++ programming_basics/03. More Conditionals/More exercise/03.Flowers.js
@@ -1,86 +1,85 @@
-function flowers(input){
+function flowers(input) {
 
-let numChrysanthemumsPurchased = Number(input[0]);
-let numRosesPurchased = Number(input[1]);
-let numTulipsPurchased = Number(input[2]);
-let season = input[3];
-let holidayOrNot = input[4];
+    let numChrysanthemumsPurchased = Number(input[0]);
+    let numRosesPurchased = Number(input[1]);
+    let numTulipsPurchased = Number(input[2]);
+    let season = input[3];
+    let holidayOrNot = input[4];
 
-let chrysanthemumsPrice = 0;
-let rosesPrice = 0;
-let tulipsPrice = 0;
-let totalPrice = 0;
-let discount = 0;
-let holidayPrice = 0;
+    let chrysanthemumsPrice = 0;
+    let rosesPrice = 0;
+    let tulipsPrice = 0;
+    let totalPrice = 0;
+    let discount = 0;
 
-let totalNum = numChrysanthemumsPurchased + numRosesPurchased + numTulipsPurchased;
+    let totalNum = numChrysanthemumsPurchased + numRosesPurchased + numTulipsPurchased;
 
-let priceToArrange = 2.00;
-let pricePerBouquet = 0;
+    let priceToArrange = 2.00;
+    let pricePerBouquet = 0;
 
-switch(season){
-    case 'Spring':
-        chrysanthemumsPrice = 2.00;
-        rosesPrice = 4.10;
-        tulipsPrice = 2.50;
-        totalPrice = (numChrysanthemumsPurchased*chrysanthemumsPrice)+(numRosesPurchased*rosesPrice)+(numTulipsPurchased*tulipsPrice);
-        if(holidayOrNot == 'Y'){
-            holidayPrice = totalPrice + 0.15 * totalPrice;
-            if(numTulipsPurchased > 7){
-             holidayPrice = totalPrice * (5 / 100);
-            }else if(totalNum > 20){
-              holidayPrice = holidayPrice * (20 / 100);
+    switch (season) {
+        case 'Spring':
+            chrysanthemumsPrice = 2.00;
+            rosesPrice = 4.10;
+            tulipsPrice = 2.50;
+            totalPrice = (numChrysanthemumsPurchased * chrysanthemumsPrice) + (numRosesPurchased * rosesPrice) + (numTulipsPurchased * tulipsPrice);
+            if (holidayOrNot == 'Y') {
+                totalPrice = totalPrice + 0.15 * totalPrice;
+                if (numTulipsPurchased > 7) {
+                    discount = totalPrice * (5 / 100);
+                } else if (totalNum > 20) {
+                    discount = totalPrice * (20 / 100);
+                }
             }
-        }
-        pricePerBouquet = discount + priceToArrange;
-        console.log(pricePerBouquet.toFixed(2));
-        break;
-    case 'Summer':
-        chrysanthemumsPrice = 2.00;
-        rosesPrice = 4.10;
-        tulipsPrice = 2.50;
-        totalPrice = (numChrysanthemumsPurchased*chrysanthemumsPrice)+(numRosesPurchased*rosesPrice)+(numTulipsPurchased*tulipsPrice);
-        if(holidayOrNot == 'Y'){
-            totalPrice = totalPrice + 0.15 * totalPrice;
-            if(totalNum > 20){
-                discount = totalPrice * (20 / 100);
+            pricePerBouquet = discount + priceToArrange;
+            console.log(pricePerBouquet.toFixed(2));
+            break;
+        case 'Summer':
+            chrysanthemumsPrice = 2.00;
+            rosesPrice = 4.10;
+            tulipsPrice = 2.50;
+            totalPrice = (numChrysanthemumsPurchased * chrysanthemumsPrice) + (numRosesPurchased * rosesPrice) + (numTulipsPurchased * tulipsPrice);
+            if (holidayOrNot == 'Y') {
+                totalPrice = totalPrice + 0.15 * totalPrice;
+                if (totalNum > 20) {
+                    discount = totalPrice * (20 / 100);
+                }
             }
-        }
-        pricePerBouquet = discount + priceToArrange;
-        console.log(pricePerBouquet.toFixed(2));
-        break;
-    case 'Autmn':
-        chrysanthemumsPrice = 3.75;
-        rosesPrice = 4.50;
-        tulipsPrice = 4.15;
-        totalPrice = (numChrysanthemumsPurchased*chrysanthemumsPrice)+(numRosesPurchased*rosesPrice)+(numTulipsPurchased*tulipsPrice);
-        if(holidayOrNot == 'Y'){
-            totalPrice = totalPrice + 0.15 * totalPrice;
-            if(totalNum > 20){
-                discount = totalPrice * (20 / 100);
+            pricePerBouquet = discount + priceToArrange;
+            console.log(pricePerBouquet.toFixed(2));
+            break;
+        case 'Autmn':
+            chrysanthemumsPrice = 3.75;
+            rosesPrice = 4.50;
+            tulipsPrice = 4.15;
+            totalPrice = (numChrysanthemumsPurchased * chrysanthemumsPrice) + (numRosesPurchased * rosesPrice) + (numTulipsPurchased * tulipsPrice);
+            if (holidayOrNot == 'Y') {
+                totalPrice = totalPrice + 0.15 * totalPrice;
+                if (totalNum > 20) {
+                    discount = totalPrice * (20 / 100);
+                }
             }
-        }
-        pricePerBouquet = discount + priceToArrange;
-        console.log(pricePerBouquet.toFixed(2));
-        break;
-    case 'Winter':
-        chrysanthemumsPrice = 3.75;
-        rosesPrice = 4.50;
-        tulipsPrice = 4.15;
-        totalPrice = (numChrysanthemumsPurchased*chrysanthemumsPrice)+(numRosesPurchased*rosesPrice)+(numTulipsPurchased*tulipsPrice);
-        if(holidayOrNot == 'Y'){
-            totalPrice = totalPrice + 0.15 * totalPrice;
-            if(numRosesPurchased >= 10){
-                discount = totalPrice * (10 / 100);
-            }else if(totalNum > 20){
-                discount = totalPrice * (20 / 100);
+            pricePerBouquet = discount + priceToArrange;
+            console.log(pricePerBouquet.toFixed(2));
+            break;
+        case 'Winter':
+            chrysanthemumsPrice = 3.75;
+            rosesPrice = 4.50;
+            tulipsPrice = 4.15;
+            totalPrice = (numChrysanthemumsPurchased * chrysanthemumsPrice) + (numRosesPurchased * rosesPrice) + (numTulipsPurchased * tulipsPrice);
+            if (holidayOrNot == 'Y') {
+                totalPrice = totalPrice + 0.15 * totalPrice;
+                if (numRosesPurchased >= 10) {
+                    discount = totalPrice * (10 / 100);
+                } else if (totalNum > 20) {
+                    discount = totalPrice * (20 / 100);
+                }
             }
-        }
-        pricePerBouquet = discount + priceToArrange;
-        console.log(pricePerBouquet.toFixed(2));
-        break;
+            pricePerBouquet = discount + priceToArrange;
+            console.log(pricePerBouquet.toFixed(2));
+            break;
+    }
+
+
 }
-
-
-}
-flowers(["2","4","8","Spring","Y"]);
+flowers(["2", "4", "8", "Spring", "Y"]);
