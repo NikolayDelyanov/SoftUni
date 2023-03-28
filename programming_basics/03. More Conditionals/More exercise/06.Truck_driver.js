49b8932a27b837fa71486a252e809823a55b31f7
NikolayDelyanov <125092041+NikolayDelyanov@users.noreply.github.com>
2023-03-28 16:57:58 +0300

    06. Truck Driver

--- programming_basics/03. More Conditionals/More exercise/06.Truck_driver.js
+++ programming_basics/03. More Conditionals/More exercise/06.Truck_driver.js
@@ -1,65 +1,65 @@
-function truckDriver(input){
+function truckDriver(input) {
 
-let season = input[0];
-let monthKilometers = Number(input[1]);
+    let season = input[0];
+    let monthKilometers = Number(input[1]);
 
-let oneSeason = 4;
-let oneMonthSalary = 0;
-let tax = 0;
-let totalSalary = 0;
+    let oneSeason = 4;
+    let oneMonthSalary = 0;
+    let tax = 0;
+    let totalSalary = 0;
 
-switch(season){
-    case 'Spring':
-    case 'Autumn':
-        if(monthKilometers <= 5000){
-            oneMonthSalary = monthKilometers * 0.75;
-            totalSalary = oneMonthSalary * oneSeason;
-            tax = totalSalary * (10 / 100);
-        }else if(monthKilometers <= 10000){
-            oneMonthSalary = monthKilometers * 0.95;
-            totalSalary = oneMonthSalary * oneSeason;
-            tax = totalSalary * (10 / 100);
-        }else if(monthKilometers <= 20000){
-            oneMonthSalary = monthKilometers * 1.45;
-            totalSalary = oneMonthSalary * oneSeason;
-            tax = totalSalary * (10 / 100);
-        }
-        console.log(tax.toFixed(2));
-        break;
-    case 'Summer':
-        if(monthKilometers <= 5000){
-            oneMonthSalary = monthKilometers * 0.90;
-            totalSalary = oneMonthSalary * oneSeason;
-            tax = totalSalary - (0.1 * totalSalary);
-        }else if(monthKilometers <= 10000){
-            oneMonthSalary = monthKilometers * 1.10;
-            totalSalary = oneMonthSalary * oneSeason;
-            tax = (totalSalary - 0.1) * totalSalary;
-        }else if(monthKilometers <= 20000){
-            oneMonthSalary = monthKilometers * 1.45;
-            totalSalary = oneMonthSalary * oneSeason;
-            tax = (totalSalary - 0.1) * totalSalary;
-        }
-        console.log(tax.toFixed(2));
-        break;
-    case 'Winter':
-        if(monthKilometers <= 5000){
-            oneMonthSalary = monthKilometers * 1.05;
-            totalSalary = oneMonthSalary * oneSeason;
-            tax = totalSalary * (10 / 100);
-        }else if(monthKilometers <= 10000){
-            oneMonthSalary = monthKilometers * 1.25;
-            totalSalary = oneMonthSalary * oneSeason;
-            tax = totalSalary * (10 / 100);
-        }else if(monthKilometers <= 20000){
-            oneMonthSalary = monthKilometers * 1.45;
-            totalSalary = oneMonthSalary * oneSeason;
-            tax = totalSalary * (10 / 100);
-        }
-        console.log(tax.toFixed(2));
-        break;
+    switch (season) {
+        case 'Spring':
+        case 'Autumn':
+            if (monthKilometers <= 5000) {
+                oneMonthSalary = monthKilometers * 0.75;
+                totalSalary = oneMonthSalary * oneSeason;
+                tax = totalSalary - (0.1 * totalSalary);
+            } else if (monthKilometers <= 10000) {
+                oneMonthSalary = monthKilometers * 0.95;
+                totalSalary = oneMonthSalary * oneSeason;
+                tax = totalSalary - (0.1 * totalSalary);
+            } else if (monthKilometers <= 20000) {
+                oneMonthSalary = monthKilometers * 1.45;
+                totalSalary = oneMonthSalary * oneSeason;
+                tax = totalSalary - (0.1 * totalSalary);
+            }
+            console.log(tax.toFixed(2));
+            break;
+        case 'Summer':
+            if (monthKilometers <= 5000) {
+                oneMonthSalary = monthKilometers * 0.90;
+                totalSalary = oneMonthSalary * oneSeason;
+                tax = totalSalary - (0.1 * totalSalary);
+            } else if (monthKilometers <= 10000) {
+                oneMonthSalary = monthKilometers * 1.10;
+                totalSalary = oneMonthSalary * oneSeason;
+                tax = totalSalary - (0.1 * totalSalary);
+            } else if (monthKilometers <= 20000) {
+                oneMonthSalary = monthKilometers * 1.45;
+                totalSalary = oneMonthSalary * oneSeason;
+                tax = totalSalary - (0.1 * totalSalary);
+            }
+            console.log(tax.toFixed(2));
+            break;
+        case 'Winter':
+            if (monthKilometers <= 5000) {
+                oneMonthSalary = monthKilometers * 1.05;
+                totalSalary = oneMonthSalary * oneSeason;
+                tax = totalSalary - (0.1 * totalSalary);
+            } else if (monthKilometers <= 10000) {
+                oneMonthSalary = monthKilometers * 1.25;
+                totalSalary = oneMonthSalary * oneSeason;
+                tax = totalSalary - (0.1 * totalSalary);
+            } else if (monthKilometers <= 20000) {
+                oneMonthSalary = monthKilometers * 1.45;
+                totalSalary = oneMonthSalary * oneSeason;
+                tax = totalSalary - (0.1 * totalSalary);
+            }
+            console.log(tax.toFixed(2));
+            break;
+    }
+
+
 }
-
-
-}
-truckDriver(["Summer","3455"]);
+truckDriver(["Winter", "4350"]);
