2cde019534b6b1885b91a3fc27b3f83396648ad6
NikolayDelyanov <125092041+NikolayDelyanov@users.noreply.github.com>
2023-03-31 16:25:17 +0300

    01.Back to the past
    Work in progress

--- programming_basics/04. Loop Operator/exercise/More exercise/01.Back_to_the_past.js
+++ programming_basics/04. Loop Operator/exercise/More exercise/01.Back_to_the_past.js
@@ -5,15 +5,22 @@ let year = Number(input[1]);
 
 let ivanchoAge = 18;
 
+
 for(let i = 1800; i <= year; i++){
-    let currentYear = input[i];
-    if(year % 2 === 0){
+    if(i % 2 === 0){
         inheritedMoney -= 12000;
     }else{
         inheritedMoney -= 12000 + (50 * (ivanchoAge + 1));
     }
 }
 
+let remainder = inheritedMoney;
+
+if(inheritedMoney >= remainder){
+    console.log(`Yes! He will live a carefree life and will have ${(Math.abs(remainder.toFixed(2)))} dollars left.`);
+}else {
+    console.log(`He will need ${(Math.abs(remainder.toFixed(2)))} dollars to survive.`);
+}
 
 }
-backToThePast(["50000","1802"]);
+backToThePast(["100000.15","1808"]);
