0305ef372550b30b78c0008313e99451db31e6e8
NikolayDelyanov <125092041+NikolayDelyanov@users.noreply.github.com>
2023-03-31 23:47:19 +0300

    04. Even powers of 2

--- programming_basics/04. Loop Operator/exercise/More exercise/More basic lab/04.Even_powers_of_2.js
+++ programming_basics/04. Loop Operator/exercise/More exercise/More basic lab/04.Even_powers_of_2.js
@@ -1,11 +1,10 @@
-function evenPowersOf2(input){
+function evenPowersOf2(input) {
 
-let n = Number(input[0]);
+    let n = Number(input[0]);
 
-for(let i = 0; i <= n; i += 2){
-    console.log(Math.pow(2,i));
-}
-
+    for (let i = 0; i <= n; i += 2) {
+        console.log(Math.pow(2, i));
+    }
 
 }
 evenPowersOf2(["3"]);
