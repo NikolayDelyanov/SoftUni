112f6ee4427aa2cf094859e288702a5270ba7928
NikolayDelyanov <125092041+NikolayDelyanov@users.noreply.github.com>
2023-03-31 14:31:24 +0300

    01. back to the past
    not finished/process in work...

---
+++ programming_basics/04. Loop Operator/exercise/More exercise/01.Back_to_the_past.js
@@ -0,0 +1,20 @@
+function backToThePast(input){
+
+let inheritedMoney = Number(input[0]);
+let year = Number(input[1]);
+
+let ivanchoAge = 18;
+
+for(let i = 1800; i <= year; i++){
+    if(year % 2 === 0){
+        inheritedMoney -= 12000;
+    }else{
+        input[i + 1];
+        inheritedMoney -= 12000 + (50 * (ivanchoAge + 1));
+    
+    }
+}
+
+
+}
+backToThePast(["50000","1802"]);
