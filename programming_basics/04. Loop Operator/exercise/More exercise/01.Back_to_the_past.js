b5530874c51b464515b7d81b2adfd830f1042b01
NikolayDelyanov <125092041+NikolayDelyanov@users.noreply.github.com>
2023-03-31 14:36:32 +0300

    01. back tp the past

--- programming_basics/04. Loop Operator/exercise/More exercise/01.Back_to_the_past.js
+++ programming_basics/04. Loop Operator/exercise/More exercise/01.Back_to_the_past.js
@@ -6,12 +6,11 @@ let year = Number(input[1]);
 let ivanchoAge = 18;
 
 for(let i = 1800; i <= year; i++){
+    let currentYear = input[i];
     if(year % 2 === 0){
         inheritedMoney -= 12000;
     }else{
-        input[i + 1];
         inheritedMoney -= 12000 + (50 * (ivanchoAge + 1));
-    
     }
 }
 
