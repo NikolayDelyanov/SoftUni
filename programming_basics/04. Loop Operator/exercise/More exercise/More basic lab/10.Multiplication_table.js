a7b5d074474ed99fb4042b68a426fea30506e702
NikolayDelyanov <125092041+NikolayDelyanov@users.noreply.github.com>
2023-04-02 15:42:12 +0300

    10. Multiplication table

---
+++ programming_basics/04. Loop Operator/exercise/More exercise/More basic lab/10.Multiplication_table.js
@@ -0,0 +1,14 @@
+function multiplicationTable(input){
+
+let num = Number(input[0]);
+
+let totalWork = 0;
+
+for(let i = 1; i <= 10; i++){
+    totalWork = (i * num);
+    console.log(`${i} * ${num} = ${totalWork}`);
+}
+
+
+}
+multiplicationTable(["5"]);
