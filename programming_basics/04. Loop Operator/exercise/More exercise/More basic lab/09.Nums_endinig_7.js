6376f0120144d734359ec94071d302ca5281d6dc
NikolayDelyanov <125092041+NikolayDelyanov@users.noreply.github.com>
2023-04-02 15:33:27 +0300

    09.Nums ending 7
    File formating

--- programming_basics/04. Loop Operator/exercise/More exercise/More basic lab/09.Nums_endinig_7.js
+++ programming_basics/04. Loop Operator/exercise/More exercise/More basic lab/09.Nums_endinig_7.js
@@ -1,9 +1,8 @@
-function numsEnd7(input){
+function numsEnd7(input) {
 
-for(let i = 7; i <= 997; i += 10){
-    console.log(i);
-}
-
+    for (let i = 7; i <= 997; i += 10) {
+        console.log(i);
+    }
 
 }
 numsEnd7([""]);
