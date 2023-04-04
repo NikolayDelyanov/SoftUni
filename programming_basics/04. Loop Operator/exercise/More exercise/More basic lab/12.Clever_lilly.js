4e4813012662d3788ff7f399ebf45dd2930c0673
NikolayDelyanov <125092041+NikolayDelyanov@users.noreply.github.com>
2023-04-04 16:54:56 +0300

    12. Clever Lilly

--- programming_basics/04. Loop Operator/exercise/More exercise/More basic lab/12.Clever_lilly.js
+++ programming_basics/04. Loop Operator/exercise/More exercise/More basic lab/12.Clever_lilly.js
@@ -1,37 +1,36 @@
-function cleverLilly(input){
+function cleverLilly(input) {
 
-let years = Number(input[0]);
-let washingMachinePrice = Number(input[1]);
-let toyPrice = Number(input[2]);
+    let years = Number(input[0]);
+    let washingMachinePrice = Number(input[1]);
+    let toyPrice = Number(input[2]);
 
-let toysCounter = 0;
-let savedMoney = 0;
-let stolenMoney = 0;
-let addMoney = 10;
+    let toysCounter = 0;
+    let savedMoney = 0;
+    let stolenMoney = 0;
+    let addMoney = 10;
 
-for(let currentAge = 1; currentAge <= years; currentAge++){
-    if(currentAge % 2 === 0){
-        savedMoney += addMoney;
-        addMoney += 10;
-        stolenMoney++;
-    }else {
-        toysCounter++;
+    for (let currentAge = 1; currentAge <= years; currentAge++) {
+        if (currentAge % 2 === 0) {
+            savedMoney += addMoney;
+            addMoney += 10;
+            stolenMoney++;
+        } else {
+            toysCounter++;
+        }
     }
-}
 
-let totalSavedMoneyFromToys = toyPrice * toysCounter;
-let totalSavedMoney = totalSavedMoneyFromToys + savedMoney - stolenMoney;
+    let totalSavedMoneyFromToys = toyPrice * toysCounter;
+    let totalSavedMoney = totalSavedMoneyFromToys + savedMoney - stolenMoney;
 
-if (totalSavedMoney >= washingMachinePrice) {
-    console.log(`Yes! ${(totalSavedMoney - washingMachinePrice).toFixed(2)}`);
-} else {
-    console.log(`No! ${(washingMachinePrice - totalSavedMoney).toFixed(2)}`);
-}
-
+    if (totalSavedMoney >= washingMachinePrice) {
+        console.log(`Yes! ${(totalSavedMoney - washingMachinePrice).toFixed(2)}`);
+    } else {
+        console.log(`No! ${(washingMachinePrice - totalSavedMoney).toFixed(2)}`);
+    }
 
 }
 cleverLilly(["10",
 
-"170.00",
+    "170.00",
 
-"6"]);
+    "6"]);
