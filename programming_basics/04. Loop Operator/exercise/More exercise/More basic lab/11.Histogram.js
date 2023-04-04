490909dcd1090f3841ed91f49db8bc86394f6b00
NikolayDelyanov <125092041+NikolayDelyanov@users.noreply.github.com>
2023-04-04 16:25:58 +0300

    11.Histogram

--- programming_basics/04. Loop Operator/exercise/More exercise/More basic lab/11.Histogram.js
+++ programming_basics/04. Loop Operator/exercise/More exercise/More basic lab/11.Histogram.js
@@ -1,39 +1,36 @@
-function histogram(input){
+function histogram(input) {
 
-let nums = Number(input[0]);
+    let nums = Number(input[0]);
 
-let p1 = 0;
-let p2 = 0;
-let p3 = 0;
-let p4 = 0;
-let p5 = 0;
+    let p1 = 0;
+    let p2 = 0;
+    let p3 = 0;
+    let p4 = 0;
+    let p5 = 0;
 
+    for (let index = 1; index <= nums; index++) {
 
-for (let index = 1; index <= nums; index++) {
+        let currentNumber = Number(input[index]);
 
-    let currentNumber = Number(input[index]);
+        if (currentNumber < 200) {
+            p1++;
+        } else if (currentNumber <= 399) {
+            p2++;
+        } else if (currentNumber <= 599) {
+            p3++;
+        } else if (currentNumber <= 799) {
+            p4++;
+        } else if (currentNumber >= 800) {
+            p5++;
+        }
 
-    if (currentNumber < 200) {
-        p1++;
-    } else if (currentNumber <= 399) {
-        p2++;
-    } else if (currentNumber <= 599) {
-        p3++;
-    } else if (currentNumber <= 799) {
-        p4++;
-    } else if (currentNumber >= 800) {
-        p5++;
     }
 
-}
-
-console.log(`${(p1 / nums * 100).toFixed(2)}%`);
-console.log(`${(p2 / nums * 100).toFixed(2)}%`);
-console.log(`${(p3 / nums * 100).toFixed(2)}%`);
-console.log(`${(p4 / nums * 100).toFixed(2)}%`);
-console.log(`${(p5 / nums * 100).toFixed(2)}%`);
-
-
+    console.log(`${(p1 / nums * 100).toFixed(2)}%`);
+    console.log(`${(p2 / nums * 100).toFixed(2)}%`);
+    console.log(`${(p3 / nums * 100).toFixed(2)}%`);
+    console.log(`${(p4 / nums * 100).toFixed(2)}%`);
+    console.log(`${(p5 / nums * 100).toFixed(2)}%`);
 
 }
-histogram(["3","1","2","999"]);
+histogram(["3", "1", "2", "999"]);
