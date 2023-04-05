6454cee1fe76e32335376bef14c9e62939d2de42
NikolayDelyanov <125092041+NikolayDelyanov@users.noreply.github.com>
2023-04-05 14:55:19 +0300

    14. Oscars
     Formating the file...

--- programming_basics/04. Loop Operator/exercise/More exercise/More basic lab/14.Oscars.js
+++ programming_basics/04. Loop Operator/exercise/More exercise/More basic lab/14.Oscars.js
@@ -1,38 +1,36 @@
-function oscars(input){
+function oscars(input) {
 
-let actorName = input[0];
-let academyPoints = Number(input[1]);
-let numberOfAssessors = Number(input[2]);
+    let actorName = input[0];
+    let academyPoints = Number(input[1]);
+    let numberOfAssessors = Number(input[2]);
 
-for (let index = 3; index < input.length; index += 2){
-    let assessorName = input[index];
-    let assessorPoints = input[index + 1];
+    for (let index = 3; index < input.length; index += 2) {
+        let assessorName = input[index];
+        let assessorPoints = input[index + 1];
 
-    let totalPoints = (assessorName.length * assessorPoints) / 2;
+        let totalPoints = (assessorName.length * assessorPoints) / 2;
 
-    academyPoints += totalPoints;
-    
-if(academyPoints > 1250.5){
-    console.log(`Congratulations, ${actorName} got a nominee for leading role with ${academyPoints.toFixed(1)}!`);
-    break;
-}
-}
+        academyPoints += totalPoints;
 
+        if (academyPoints > 1250.5) {
+            console.log(`Congratulations, ${actorName} got a nominee for leading role with ${academyPoints.toFixed(1)}!`);
+            break;
+        }
+    }
 
- if(academyPoints < 1250.5) {
-    console.log(`Sorry, ${actorName} you need ${(1250.5 - academyPoints).toFixed(1)} more!`);
-}
-
+    if (academyPoints < 1250.5) {
+        console.log(`Sorry, ${actorName} you need ${(1250.5 - academyPoints).toFixed(1)} more!`);
+    }
 
 }
 oscars(["Zahari Baharov",
-"205",
-"4",
-"Johnny Depp",
-"45",
-"Will Smith",
-"29",
-"Jet Lee",
-"10",
-"Matthew Mcconaughey",
-"39"]);
+    "205",
+    "4",
+    "Johnny Depp",
+    "45",
+    "Will Smith",
+    "29",
+    "Jet Lee",
+    "10",
+    "Matthew Mcconaughey",
+    "39"]);
