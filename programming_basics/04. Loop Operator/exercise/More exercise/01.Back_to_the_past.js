a0184f11da193243f10aefdd2774fd06c57b5fda
NikolayDelyanov <125092041+NikolayDelyanov@users.noreply.github.com>
2023-04-05 16:16:58 +0300

    01.Back to the past

--- programming_basics/04. Loop Operator/exercise/More exercise/01.Back_to_the_past.js
+++ programming_basics/04. Loop Operator/exercise/More exercise/01.Back_to_the_past.js
@@ -5,33 +5,24 @@ let year = Number(input[1]);
 
 let ivanchoAge = 18;
 
-let remainder = inheritedMoney;
-
 for(let i = 1800; i <= year; i++){
     if(i % 2 === 0){
         inheritedMoney -= 12000;
     }else{
         inheritedMoney -= 12000 + (50 * (ivanchoAge + 1));
     }
-
-    if(inheritedMoney >= remainder){
-        console.log(`Yes! He will live a carefree life and will have ${(Math.abs(remainder.toFixed(2)))} dollars left.`);
-    }else {
-        console.log(`He will need ${(Math.abs(remainder.toFixed(2)))} dollars to survive.`);
-    }
 }
 
 for(let i = 1800; i <= year; i++){
+    let n = inheritedMoney;
     if(i % 2 === 0){
-       inheritedMoney -= 12000;
-       break;
+       n -= 12000;
     }else{
-        inheritedMoney -= 12000 + (50 * (ivanchoAge + 1));
-        break;
+        n -= 12000 + (50 * (ivanchoAge + 1));
     }
-
 }
 
+let remainder = inheritedMoney;
 
 if(inheritedMoney >= remainder){
     console.log(`Yes! He will live a carefree life and will have ${(Math.abs(remainder.toFixed(2)))} dollars left.`);
