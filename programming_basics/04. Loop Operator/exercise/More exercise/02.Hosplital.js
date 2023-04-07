dd437693a181a680f46df7585ab5f579b9f75422
NikolayDelyanov <125092041+NikolayDelyanov@users.noreply.github.com>
2023-04-07 17:39:37 +0300

    02. Hospital
     Work in progress

--- programming_basics/04. Loop Operator/exercise/More exercise/02.Hosplital.js
+++ programming_basics/04. Loop Operator/exercise/More exercise/02.Hosplital.js
@@ -1,23 +1,34 @@
 function hospital(input) {
 
     let periodOfTime = Number(input[0]);
-    let numOfPatients = Number(input[1]);
 
-    let numDoctors = 7;
-    let numExamined = 0;
-    let numUnexamined = 0;
+
 
     for (let index = 1; index <= input.length; index++) {
         let numPatientsPerDay = input[index];
 
-        if (numDoctors >= numPatientsPerDay) {
-            numExamined += numPatientsPerDay;
-            numDoctors -= numPatientsPerDay;
-        } else {
-            numExamined += numDoctors;
-            numUnexamined += numPatientsPerDay;
-        }
+        let numDoctors = 7;
+        let numExamined = 0;
+        let numUnexamined = 0;
 
+        if (numDoctors >= numPatientsPerDay) {
+            numExamined = numPatientsPerDay;
+            numDoctors -= numPatientsPerDay;
+
+        } else {
+            numExamined = numDoctors;
+            numUnexamined = numPatientsPerDay - numExamined;
+        }
+    }
+    for (let index = 3; index <= input.length; index += 3) {
+        let numPatientsPerDay = input[index];
+
+        if (index % 3 === 0 && numUnexamined > numExamined){
+            numDoctors += 1;
+            numExamined = numDoctors;
+            numUnexamined = numPatientsPerDay - numExamined;
+        }
+        
     }
 
 }
