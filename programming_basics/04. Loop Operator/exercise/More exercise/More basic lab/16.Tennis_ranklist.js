009f7f72c2d8805285fd518f1093d6e9322d0a25
NikolayDelyanov <125092041+NikolayDelyanov@users.noreply.github.com>
2023-04-08 00:41:20 +0300

    16. Tennis Ranklist

---
+++ programming_basics/04. Loop Operator/exercise/More exercise/More basic lab/16.Tennis_ranklist.js
@@ -0,0 +1,16 @@
+function tennisRanklist(input){
+
+let numTournaments = Number(input[0]);
+let startRanklistPoints = Number(input[1]);
+
+
+
+
+}
+tennisRanklist(["5",
+"1400",
+"F",
+"SF",
+"W",
+"W",
+"SF"]);
