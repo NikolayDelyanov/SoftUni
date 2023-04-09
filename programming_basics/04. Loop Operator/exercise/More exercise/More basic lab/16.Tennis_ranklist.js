de38a57c931ea64b00ba5fa4b9e4fc0d829e0833
NikolayDelyanov <125092041+NikolayDelyanov@users.noreply.github.com>
2023-04-10 01:34:27 +0300

    16. Tennis Ranklist

--- programming_basics/04. Loop Operator/exercise/More exercise/More basic lab/16.Tennis_ranklist.js
+++ programming_basics/04. Loop Operator/exercise/More exercise/More basic lab/16.Tennis_ranklist.js
@@ -1,7 +1,32 @@
 function tennisRanklist(input){
 
 let numTournaments = Number(input[0]);
-let startRanklistPoints = Number(input[1]);
+let startingPoints = Number(input[1]);
+let wins = 0;
+let points = startingPoints;
+
+for (let index = 2; index <= input.length; index++) {
+    let stage = input[index];
+
+    switch(stage){
+        case 'W':
+            points += 2000;
+            wins++;
+            break;
+        case 'F':
+            points += 1200;
+            break;
+        case 'SF':
+            points += 720;
+            break;
+    }
+
+}
+let avgPoints = (points - startingPoints) / numTournaments;
+
+console.log(`Final points: ${points}`);
+console.log(`Average points: ${Math.floor(avgPoints)}`);
+console.log(`${(wins / numTournaments * 100).toFixed(2)}%`);
 
 
 
