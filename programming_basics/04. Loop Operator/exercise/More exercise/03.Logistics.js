e0ace94cc1105e927a4ea54ad72efd82d855770c
NikolayDelyanov <125092041+NikolayDelyanov@users.noreply.github.com>
2023-04-10 01:09:14 +0300

    03. Logistics

---
+++ programming_basics/04. Loop Operator/exercise/More exercise/03.Logistics.js
@@ -0,0 +1,18 @@
+function logisticts(input) {
+
+    let cargoNum = Number(input[0]);
+
+    for (let index = 1; index <= input.length; index++) {
+        let tonnage = Number(input[index]);
+
+        let vehicle = 0;
+        let totalCargo = (input[index + 1]);
+        let averagePerTon = 0;
+
+    
+
+    }
+
+
+}
+logisticts(["4", "1", "5", "16", "3"]);
