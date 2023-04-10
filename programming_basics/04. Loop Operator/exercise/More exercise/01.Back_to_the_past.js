e07a3ba96ae7a77b6ec9fcf10e7e9767504c2888
NikolayDelyanov <125092041+NikolayDelyanov@users.noreply.github.com>
2023-04-11 00:55:34 +0300

    01. Back to the past

--- programming_basics/04. Loop Operator/exercise/More exercise/01.Back_to_the_past.js
+++ programming_basics/04. Loop Operator/exercise/More exercise/01.Back_to_the_past.js
@@ -3,30 +3,31 @@ function backToThePast(input){
 let inheritanceMoney = Number(input[0]);
 let year = Number(input[1]);
 
+let currentAge = 18;
 let spentMoney = 0;
+let currentMoney = inheritanceMoney;
 
 for (let index = 1800; index <= year ; index++) {
-    let currentAge = 18;
-
-    if(index % 3 === 0){
-        inheritanceMoney -= spentMoney;
-    }
-
+    
     if(index % 2 === 0){
         inheritanceMoney -= 12000;
     }else {
-        currentAge++;
         spentMoney = 12000 + (50 * currentAge);
         inheritanceMoney -= spentMoney;
+    }
+
+    if(index % 3 === 0){
+        currentAge++;
+        currentMoney -= spentMoney;
     }
 
 }
 
 if(inheritanceMoney > 0){
-    console.log(`Yes! He will live a carefree life and will have ${(Math.abs(inheritanceMoney.toFixed(2)))} dollars left.`);
+    console.log(`Yes! He will live a carefree life and will have ${(Math.abs(inheritanceMoney)).toFixed(2)} dollars left.`);
 }else{
-    console.log(`He will need ${(Math.abs(inheritanceMoney.toFixed(2)))} dollars to survive.`);
+    console.log(`He will need ${(Math.abs(inheritanceMoney)).toFixed(2)} dollars to survive.`);
 }
 
 }
-backToThePast(["100000.15","1808"]);
+backToThePast(["50000","1802"]);
