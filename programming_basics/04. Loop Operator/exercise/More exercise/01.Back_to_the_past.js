823329e6e18e00c2dac9283e2c917607555678e4
NikolayDelyanov <125092041+NikolayDelyanov@users.noreply.github.com>
2023-04-11 00:57:30 +0300

    01. Back to the past

--- programming_basics/04. Loop Operator/exercise/More exercise/01.Back_to_the_past.js
+++ programming_basics/04. Loop Operator/exercise/More exercise/01.Back_to_the_past.js
@@ -1,33 +1,32 @@
-function backToThePast(input){
+function backToThePast(input) {
 
-let inheritanceMoney = Number(input[0]);
-let year = Number(input[1]);
+    let inheritanceMoney = Number(input[0]);
+    let year = Number(input[1]);
 
-let currentAge = 18;
-let spentMoney = 0;
-let currentMoney = inheritanceMoney;
+    let currentAge = 18;
+    let spentMoney = 0;
+    let currentMoney = inheritanceMoney;
 
-for (let index = 1800; index <= year ; index++) {
-    
-    if(index % 2 === 0){
-        inheritanceMoney -= 12000;
-    }else {
-        spentMoney = 12000 + (50 * currentAge);
-        inheritanceMoney -= spentMoney;
+    for (let index = 1800; index <= year; index++) {
+
+        if (index % 2 === 0) {
+            inheritanceMoney -= 12000;
+        } else {
+            spentMoney = 12000 + (50 * currentAge);
+            inheritanceMoney -= spentMoney;
+        }
+
+        if (index % 3 === 0) {
+            currentAge++;
+            currentMoney -= spentMoney;
+        }
     }
 
-    if(index % 3 === 0){
-        currentAge++;
-        currentMoney -= spentMoney;
+    if (inheritanceMoney > 0) {
+        console.log(`Yes! He will live a carefree life and will have ${(Math.abs(inheritanceMoney)).toFixed(2)} dollars left.`);
+    } else {
+        console.log(`He will need ${(Math.abs(inheritanceMoney)).toFixed(2)} dollars to survive.`);
     }
 
 }
-
-if(inheritanceMoney > 0){
-    console.log(`Yes! He will live a carefree life and will have ${(Math.abs(inheritanceMoney)).toFixed(2)} dollars left.`);
-}else{
-    console.log(`He will need ${(Math.abs(inheritanceMoney)).toFixed(2)} dollars to survive.`);
-}
-
-}
-backToThePast(["50000","1802"]);
+backToThePast(["50000", "1802"]);
