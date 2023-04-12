9fcc91d9de732c4abbfedfbf7f34cbabcd50551f
NikolayDelyanov <125092041+NikolayDelyanov@users.noreply.github.com>
2023-04-12 19:41:39 +0300

    01.Agency profit

--- programming_basics/PB Exams/01.Agency_profit.js
+++ programming_basics/PB Exams/01.Agency_profit.js
@@ -1,22 +1,20 @@
-function agencyProfit(input){
+function agencyProfit(input) {
 
-let airlaneName = input[0];
-let ticketsForAdults = Number(input[1]);
-let ticketsForChilds = Number(input[2]);
-let netPriceForAdults = Number(input[3]);
-let chargePrice = Number(input[4]);
+    let airlaneName = input[0];
+    let ticketsForAdults = Number(input[1]);
+    let ticketsForChilds = Number(input[2]);
+    let netPriceForAdults = Number(input[3]);
+    let chargePrice = Number(input[4]);
 
-let netPriceForChilds = netPriceForAdults - netPriceForAdults * 0.70;
-let adultTicketWithCharge = netPriceForAdults + chargePrice;
-let childTicketWithCharge = netPriceForChilds + chargePrice;
+    let netPriceForChilds = netPriceForAdults - netPriceForAdults * 0.70;
+    let adultTicketWithCharge = netPriceForAdults + chargePrice;
+    let childTicketWithCharge = netPriceForChilds + chargePrice;
 
-let totalPrice = (ticketsForChilds * childTicketWithCharge) + (ticketsForAdults * adultTicketWithCharge);
+    let totalPrice = (ticketsForChilds * childTicketWithCharge) + (ticketsForAdults * adultTicketWithCharge);
 
-let profit = totalPrice * (20 / 100);
+    let profit = totalPrice * (20 / 100);
 
-console.log(`The profit of your agency from ${airlaneName} tickets is ${profit.toFixed(0)} lv.`);
-
-
+    console.log(`The profit of your agency from ${airlaneName} tickets is ${profit.toFixed(2)} lv.`);
 
 }
-agencyProfit(["WizzAir","15","5","120","40"]);
+agencyProfit(["WizzAir", "15", "5", "120", "40"]);
