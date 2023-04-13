984d462f72ff80b2d0fbeafb2f92d63a20ceffac
NikolayDelyanov <125092041+NikolayDelyanov@users.noreply.github.com>
2023-04-13 13:18:43 +0300

    02.Add bags

---
+++ programming_basics/PB Exams/02.Add_bags.js
@@ -0,0 +1,49 @@
+function addBags(input){
+
+let luggagePriceOver20 = Number(input[0]);
+let luggageKilogram = Number(input[1]);
+let days = Number(input[2]);
+let numLuggage = Number(input[3]);
+
+let priceLagguge = 0;
+
+if(luggageKilogram < 10){
+    priceLagguge = luggagePriceOver20 * (20 / 100);
+    
+    if(days > 30){
+        priceLagguge = priceLagguge + 0.10 * priceLagguge;
+    }else if(days >= 7 && days <= 30){
+        priceLagguge = priceLagguge + 0.15 * priceLagguge;
+    }else if(days < 7){
+        priceLagguge = priceLagguge + 0.40 * priceLagguge;
+    }
+
+}else if(luggageKilogram >= 10 && luggageKilogram <= 20){
+    priceLagguge = luggagePriceOver20 * (50 / 100);
+
+    if(days > 30){
+        priceLagguge = priceLagguge + 0.10 * priceLagguge;
+    }else if(days >= 7 && days <= 30){
+        priceLagguge = priceLagguge + 0.15 * priceLagguge;
+    }else if(days < 7){
+        priceLagguge = priceLagguge + 0.40 * priceLagguge;
+    }
+
+}else if(luggageKilogram > 20){
+    console.log(priceLagguge);
+    
+    if(days > 30){
+        priceLagguge = priceLagguge + 0.10 * priceLagguge;
+    }else if(days >= 7 && days <= 30){
+        priceLagguge = priceLagguge + 0.15 * priceLagguge;
+    }else if(days < 7){
+        priceLagguge = priceLagguge + 0.40 * priceLagguge;
+    }
+}
+
+let totalSum = priceLagguge * numLuggage;
+
+console.log(`The total price of bags is: ${totalSum.toFixed(2)} lv.`);
+
+}
+addBags(["63.80","23","3","1"]);
