8d824fdf46dc8627d68d407cb485fccc03a093d5
NikolayDelyanov <125092041+NikolayDelyanov@users.noreply.github.com>
2023-04-14 14:56:18 +0300

    04.Balls

--- programming_basics/PB Exams/04.Balls.js
+++ programming_basics/PB Exams/04.Balls.js
@@ -4,34 +4,49 @@ let ballsNum = Number(input[0]);
 
 let points = 0;
 
+let numRedBalls = 0;
+let numOrangeBalls = 0;
+let numYellowBalls = 0;
+let numWhiteBalls = 0;
+let otherNum = 0;
+let divides = 0;
+
 for (let index = 1; index <= input.length; index++) {
     let numColor = (input[index]);
     
     if(numColor == "red"){
         points += 5;
-        console.log(`Red balls: ${Number(ballsNum - numColor)}`);
+        numRedBalls++;
 
     } else if(numColor == "orange"){
         points += 10;
-        console.log(`Orange balls: ${Number(ballsNum - numColor)}`);
+        numOrangeBalls++;
 
     } else if(numColor == "yellow"){
         points += 15;
-        console.log(`Yellow balls: ${Number(ballsNum - numColor)}`);
+        numYellowBalls++;
 
     } else if(numColor == "white"){
         points += 20;
-        console.log(`White balls: ${Number(ballsNum - numColor)}`);
-
+        numWhiteBalls++;
+    
     } else if(numColor == "black"){
         points = (points / 2);
-        console.log(`Black balls: ${Number(ballsNum - numColor)}`);
-
+        divides++;
+        
     } else {
-        console.log(`Other colors picked: ${Number(ballsNum - numColor)}`);
+        otherNum++;
+        
     }
 }
+console.log(`Total points: ${points}`);
+console.log(`Red balls: ${numRedBalls}`);
+console.log(`Orange balls: ${numOrangeBalls}`);
+console.log(`Yellow balls: ${numYellowBalls}`);
+console.log(`White balls: ${numWhiteBalls}`);
+console.log(`Other colors picked: ${otherNum - 1}`);
+console.log(`Divides from black balls: ${divides}`);
 
 
 }
-balls(["3","white","black","pink"]);
+balls(["10","white","white","ee","red","orange","red"]);
