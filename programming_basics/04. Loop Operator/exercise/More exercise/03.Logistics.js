7c2f4341b30576e2ea5a9de082709df0c9cec49c
NikolayDelyanov <125092041+NikolayDelyanov@users.noreply.github.com>
2023-04-15 01:20:48 +0300

    03. Logistics

--- programming_basics/04. Loop Operator/exercise/More exercise/03.Logistics.js
+++ programming_basics/04. Loop Operator/exercise/More exercise/03.Logistics.js
@@ -1,26 +1,20 @@
 function logisticts(input) {
 
 let loadsNum = Number(input[0]);
-let tonnage = 0;
 
-let totalWeight = 0;
-let microbusWeight = 0;
-let truckWeight = 0;
-let trainWeight = 0;
+let microbus = 0;
+let truck = 0;
+let train = 0;
+let vehicle = 0;
+
+let totalLoads = 0;
+let average = 0;
 
 for (let index = 1; index < input.length; index++) {
-    let weigth = Number(input[index]);
-    totalWeight += parseInt(weigth);
-
-    if(weigth <= 3){
-        microbusWeight += parseInt(weigth);
-    }else if(weigth >= 4 && weigth <= 11){
-        truckWeight += parseInt(weigth);
-    }else {
-        trainWeight += parseInt(weigth);
-
-    }
-   
+    let tonnage = Number(input[index]);
+    totalLoads += tonnage;
+    
+    
 }
 
 }
