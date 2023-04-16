aaf5a53eab0f1552e827008dfa835ba1c9895da8
NikolayDelyanov <125092041+NikolayDelyanov@users.noreply.github.com>
2023-04-17 01:49:34 +0300

    10.Clock 2

---
+++ programming_basics/04. Loop Operator/exercise/More exercise/10.Clock2.js
@@ -0,0 +1,11 @@
+function clock2(){
+
+    for (let hours = 0; hours < 24; hours++) {
+        for (let minutes = 0; minutes < 60; minutes++) {
+            for(let seconds = 0; seconds < 60; seconds++)
+            console.log(`${hours} : ${minutes} : ${seconds}`);
+        }
+    }
+
+}
+clock2();
