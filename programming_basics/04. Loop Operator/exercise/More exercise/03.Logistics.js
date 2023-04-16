ca1882eebbfc4b339a906578bcec574df3d36003
NikolayDelyanov <125092041+NikolayDelyanov@users.noreply.github.com>
2023-04-16 16:53:12 +0300

    03. Logistics

--- programming_basics/04. Loop Operator/exercise/More exercise/03.Logistics.js
+++ programming_basics/04. Loop Operator/exercise/More exercise/03.Logistics.js
@@ -10,11 +10,24 @@ function logisticts(input) {
     let totalLoads = 0;
     let average = 0;
 
+    let sum1 = 0;
+    let sum2 = 0;
+    let sum3 = 0;
+
     for (let index = 1; index < input.length; index++) {
         let tonnage = Number(input[index]);
         totalLoads += tonnage;
 
+        if(tonnage <= 4){
+            sum1 = input[index];
+        } else if(tonnage <= 11){
+            sum2 = input[index];
+        } else if(tonnage >= 12){
+            sum3 = input[index];
+        }
     }
+
+    average = (sum1 * 200 + sum2 * 175 + sum3 * 120)/totalLoads;
 
 }
 logisticts(["4", "1", "5", "16", "3"]);
