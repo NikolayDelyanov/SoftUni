3e9c3cd6a5be01c89ed58db7a770725b272b2a94
NikolayDelyanov <125092041+NikolayDelyanov@users.noreply.github.com>
2023-04-19 16:38:56 +0300

    04. Sequence 2k + 1

---
+++ programming_basics/05. While Loop/04.Sequence_2k+1.js
@@ -0,0 +1,16 @@
+function sequence(input){
+
+let num = Number(input[0]);
+
+let current = 1;
+while(current <= num){
+    console.log(current);
+    if(current >= num){
+        break;
+    }
+    current = (current * 2) + 1;
+}
+
+
+}
+sequence(["31"]);
