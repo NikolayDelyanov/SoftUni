ed734f0c36999e67e83558b496889a9243d50d0f
NikolayDelyanov <125092041+NikolayDelyanov@users.noreply.github.com>
2023-04-19 16:24:19 +0300

    02.Password

--- programming_basics/05. While Loop/02.Password.js
+++ programming_basics/05. While Loop/02.Password.js
@@ -1,20 +1,19 @@
-function password(input){
+function password(input) {
 
-let username = input[0];
-let password = input[1];
-let data = input[2];
+    let username = input[0];
+    let password = input[1];
+    let data = input[2];
 
-let index = 3;
-while (data !== password){
-    data = input[index];
-    index++;
-}
-console.log(`Welcome ${username}!`);
-
+    let index = 3;
+    while (data !== password) {
+        data = input[index];
+        index++;
+    }
+    console.log(`Welcome ${username}!`);
 
 }
 password(["Nakov",
-"1234",
-"Pass",
-"1324",
-"1234"]);
+    "1234",
+    "Pass",
+    "1324",
+    "1234"]);
