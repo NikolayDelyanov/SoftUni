dfc1c24c461a78aa47fc95d1b4c03c34811a556f
NikolayDelyanov <125092041+NikolayDelyanov@users.noreply.github.com>
2023-04-19 16:31:15 +0300

    03.Sum Numbers

---
+++ programming_basics/05. While Loop/03.Sum_numbers.js
@@ -0,0 +1,18 @@
+function sumNumbers(input){
+
+let num = Number(input[0]);
+let sum = 0;
+
+let index = 1;
+while(index < input.length){
+    sum += Number(input[index]);
+    if(sum >= num){
+        break;
+    }
+    index++;
+}
+console.log(sum);
+
+
+}
+sumNumbers(["100", "10", "20", "30", "40"]);
