8d68c2818b630dbd813a109cbc6d31ad2a9c9041
NikolayDelyanov <125092041+NikolayDelyanov@users.noreply.github.com>
2023-04-22 00:12:32 +0300

    05.Account balance

--- programming_basics/05. While Loop/05.Account_balance.js
+++ programming_basics/05. While Loop/05.Account_balance.js
@@ -1,28 +1,27 @@
-function balance(input){
+function balance(input) {
 
     let index = 0;
     let command = input[index];
     let sum = 0;
 
-    while(command !== "NoMoreMoney"){
+    while (command !== "NoMoreMoney") {
         let increase = Number(command);
-        if(increase < 0){
+        if (increase < 0) {
             console.log("Invalid operation!");
             break;
         }
-        
+
         console.log("Increase: " + increase.toFixed(2));
         sum += increase;
         index++;
         command = input[index];
-        
+
     }
 
     console.log("Total: " + sum.toFixed(2));
 
-
 }
 balance(["5.51",
-"69.42",
-"100",
-"NoMoreMoney"]);
+    "69.42",
+    "100",
+    "NoMoreMoney"]);
