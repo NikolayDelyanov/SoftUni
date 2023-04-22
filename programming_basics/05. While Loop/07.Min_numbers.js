4e9dd36cb01251b5b03678acf4363abf92595e50
NikolayDelyanov <125092041+NikolayDelyanov@users.noreply.github.com>
2023-04-22 12:44:32 +0300

    07.Min numbers

--- programming_basics/05. While Loop/07.Min_numbers.js
+++ programming_basics/05. While Loop/07.Min_numbers.js
@@ -1,25 +1,24 @@
-function minNumber(input){
+function minNumber(input) {
 
     let index = 0;
     let command = input[index];
     let min = command;
-    
-    while(command !== "Stop"){
+
+    while (command !== "Stop") {
         min = Number(min);
         let value = Number(command);
-        if(min > value){
+        if (min > value) {
             min = value;
         }
         index++;
         command = input[index];
-    
+
     }
 
     console.log(min);
-    
-    }
-    
-    minNumber(["100",
+
+}
+minNumber(["100",
     "99",
     "80",
     "70",
