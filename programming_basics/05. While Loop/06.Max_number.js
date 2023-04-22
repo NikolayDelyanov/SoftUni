ca5b65b57413f071b417a4df9e3a13b745c96692
NikolayDelyanov <125092041+NikolayDelyanov@users.noreply.github.com>
2023-04-22 12:39:45 +0300

    06.Max numbers

--- programming_basics/05. While Loop/06.Max_number.js
+++ programming_basics/05. While Loop/06.Max_number.js
@@ -1,24 +1,24 @@
-function maxNumber(input){
+function maxNumber(input) {
 
-let index = 0;
-let command = input[index];
-let max = command;
+    let index = 0;
+    let command = input[index];
+    let max = command;
 
-while(command !== "Stop"){
-    max = Number(max);
-    let value = Number(command);
-    if(value > max){
-        max = value;
+    while (command !== "Stop") {
+        max = Number(max);
+        let value = Number(command);
+        if (value > max) {
+            max = value;
+        }
+        index++;
+        command = input[index];
+
     }
-    index++;
-    command = input[index];
-
-}
-console.log(max);
+    console.log(max);
 
 }
 maxNumber(["100",
-"99",
-"80",
-"70",
-"Stop"]);
+    "99",
+    "80",
+    "70",
+    "Stop"]);
