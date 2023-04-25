400b9f98ea400874c5cc48759fee87e10763024c
NikolayDelyanov <125092041+NikolayDelyanov@users.noreply.github.com>
2023-04-25 16:09:45 +0300

    03.Vacation

--- programming_basics/05. While Loop/Exercise/03.Vacantion.js
+++ programming_basics/05. While Loop/Exercise/03.Vacantion.js
@@ -1,48 +1,48 @@
-function vacantion(input){
+function vacantion(input) {
 
-let needMoney = Number(input[0]);
-let currentMoney = Number(input[1]);
-let spendDays = 0;
-let days = 0;
-let index = 2;
-let currentInput = input[index];
+    let needMoney = Number(input[0]);
+    let currentMoney = Number(input[1]);
+    let spendDays = 0;
+    let days = 0;
+    let index = 2;
+    let currentInput = input[index];
 
-while (currentMoney < needMoney){
-    days++;
-    index++;
+    while (currentMoney < needMoney) {
+        days++;
+        index++;
 
-    if(currentInput === "spend"){
-        spendDays++;
+        if (currentInput === "spend") {
+            spendDays++;
 
-        if(spendDays === 5){
-            console.log("You can't save the money.");
-            console.log(days);
-            break;
+            if (spendDays === 5) {
+                console.log("You can't save the money.");
+                console.log(days);
+                break;
+            }
+            let moneyToSpend = Number(input[index]);
+            currentMoney -= moneyToSpend;
+
+            if (currentMoney < 0) {
+                currentMoney = 0;
+            }
+
+        } else if (currentInput === "save") {
+            spendDays = 0;
+            let moneyToSave = Number(input[index]);
+            currentMoney += moneyToSave;
         }
-        let moneyToSpend = Number(input[index]);
-        currentMoney -= moneyToSpend;
-
-        if(currentMoney < 0){
-            currentMoney = 0;
-        }
-
-    } else if(currentInput === "save"){
-        spendDays = 0;
-        let moneyToSave = Number(input[index]);
-        currentMoney += moneyToSave;
+        index++;
+        currentInput = input[index];
     }
-    index++;
-    currentInput = input[index];
-}
 
-if(currentMoney >= needMoney){
-    console.log(`You saved the money for ${days} days.`);
-}
+    if (currentMoney >= needMoney) {
+        console.log(`You saved the money for ${days} days.`);
+    }
 
 }
 vacantion(["2000",
-"1000",
-"spend",
-"1200",
-"save",
-"2000"]);
+    "1000",
+    "spend",
+    "1200",
+    "save",
+    "2000"]);
