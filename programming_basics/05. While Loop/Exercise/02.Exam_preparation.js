ff5e8fe996ab21c669acbdfa838416f93e03090a
NikolayDelyanov <125092041+NikolayDelyanov@users.noreply.github.com>
2023-04-25 16:08:54 +0300

    02. Exam preparation
    Not finished

---
+++ programming_basics/05. While Loop/Exercise/02.Exam_preparation.js
@@ -0,0 +1,22 @@
+function exam(input){
+
+let badGrades = Number(input[0]);
+let exc = input[index];
+let evaluation = input[index + 1];
+let totalSum = 0;
+let average = 0;
+
+let index = 1;
+while(badGrades < input[index]){
+    exc = input[index];
+    evaluation = input[index + 1];
+    totalSum += evaluation;
+    average = totalSum / 
+
+    if(input[index] === "Enough"){
+        break;
+    }
+}
+
+}
+exam
