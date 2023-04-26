aac982c6fdbbe95e5aa2001bfe587eac94a4a3b9
NikolayDelyanov <125092041+NikolayDelyanov@users.noreply.github.com>
2023-04-26 12:56:04 +0300

    06.Cake

--- programming_basics/05. While Loop/Exercise/06.Cake.js
+++ programming_basics/05. While Loop/Exercise/06.Cake.js
@@ -1,37 +1,37 @@
-function cake(input){
+function cake(input) {
 
-let lengthCake = Number(input[0]);
-let widthCake = Number(input[1]);
-let cakeSize = lengthCake * widthCake;
-let index = 2;
-let command = input[index];
-let numPieces = 0;
+    let lengthCake = Number(input[0]);
+    let widthCake = Number(input[1]);
+    let cakeSize = lengthCake * widthCake;
+    let index = 2;
+    let command = input[index];
+    let numPieces = 0;
 
 
-while(command !== 'STOP'){
-    
-    let cakeParts = Number(command);
-    numPieces += cakeParts;
+    while (command !== 'STOP') {
 
-    if(numPieces > cakeSize){
-        console.log(`No more cake left! You need ${numPieces - cakeSize} pieces more.`);
-        break;
+        let cakeParts = Number(command);
+        numPieces += cakeParts;
+
+        if (numPieces > cakeSize) {
+            console.log(`No more cake left! You need ${numPieces - cakeSize} pieces more.`);
+            break;
+        }
+
+        index++;
+        command = input[index];
     }
 
-    index++;
-    command = input[index];
-}
-
-if(command === 'STOP' && numPieces <= cakeSize){
-    console.log(`${cakeSize - numPieces} pieces are left.`);
-}
+    if (command === 'STOP' && numPieces <= cakeSize) {
+        console.log(`${cakeSize - numPieces} pieces are left.`);
+    }
 
 
 }
 cake[("10",
-"10",
-"20",
-"20",
-"20", 
-"20", 
-"21")];
+    "10",
+    "20",
+    "20",
+    "20",
+    "20",
+    "21")];
