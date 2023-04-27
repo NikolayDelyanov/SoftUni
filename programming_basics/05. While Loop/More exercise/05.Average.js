d2ae16f3206486c54c4835bb22fc4523b9cccd2e
NikolayDelyanov <125092041+NikolayDelyanov@users.noreply.github.com>
2023-04-28 00:11:47 +0300

    05.Average

--- programming_basics/05. While Loop/More exercise/05.Average.js
+++ programming_basics/05. While Loop/More exercise/05.Average.js
@@ -1,17 +1,17 @@
-function average(input){
+function average(input) {
 
-let n = Number(input[0]);
-let sum = 0;
-let result = 0;
+    let n = Number(input[0]);
+    let sum = 0;
+    let result = 0;
 
-for (let index = 1; index < input.length; index++) {
-    let element = Number(input[index])
-    sum += element;
-    result = (sum / n);
+    for (let index = 1; index < input.length; index++) {
+        let element = Number(input[index])
+        sum += element;
+        result = (sum / n);
+    }
+
+    console.log(result.toFixed(2));
+
+
 }
-
-console.log(result);
-
-
-}
-average(["4","3","2","4","2"]);
+average(["4", "3", "2", "4", "2"]);
