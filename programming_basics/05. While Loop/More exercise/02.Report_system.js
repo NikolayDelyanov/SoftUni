52b2745464922101b1db84009bdb15fec054e468
NikolayDelyanov <125092041+NikolayDelyanov@users.noreply.github.com>
2023-04-29 01:04:46 +0300

    02.Report system

---
+++ programming_basics/05. While Loop/More exercise/02.Report_system.js
@@ -0,0 +1,43 @@
+function reportSystem(input){
+
+let sum = Number(input[0]);
+let cashPayment = 0;
+let cardPayment = 0;
+let index = 1;
+let command = input[index];
+let totalSum = 0;
+
+while(command !== 'End'){
+
+    let transaction = Number(command);
+    totalSum += transaction;
+
+    if(index % 2 === 0){
+        cashPayment = transaction;
+
+        if(transaction > 100){
+            console.log("Error in transaction!");
+            continue;
+        }else{
+            console.log("Product sold!");
+        }
+
+    }else {
+        cardPayment = transaction;
+
+        if(transaction < 10){
+            console.log("Error in transaction!");
+            continue;
+        } else {
+            console.log("Product sold!");
+        }
+    }
+
+    index++;
+    command = input[index];
+
+}
+
+
+}
+reportSystem(["500","120","8","63","256","78","317"]);
