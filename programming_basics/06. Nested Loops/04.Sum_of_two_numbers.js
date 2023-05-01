5edaa74c0147969171ed83c598071edd89118890
NikolayDelyanov <125092041+NikolayDelyanov@users.noreply.github.com>
2023-05-01 13:58:25 +0300

    04.Sum of two numbers

--- programming_basics/06. Nested Loops/04.Sum_of_two_numbers.js
+++ programming_basics/06. Nested Loops/04.Sum_of_two_numbers.js
@@ -1,36 +1,35 @@
-function sumOfTwoNumbers(input){
+function sumOfTwoNumbers(input) {
 
-let start = Number(input[0]);
-let end = Number(input[1]);
-let magicNumber = Number(input[2]);
+    let start = Number(input[0]);
+    let end = Number(input[1]);
+    let magicNumber = Number(input[2]);
 
-let flag = false;
-let counter = 0;
+    let flag = false;
+    let counter = 0;
 
-for(let i = start; i <= end; i++){
-    for(let j = start; j <= end; j++){
-        let sum = i + j;
-        counter++;
+    for (let i = start; i <= end; i++) {
+        for (let j = start; j <= end; j++) {
+            let sum = i + j;
+            counter++;
 
-        if(sum == magicNumber){
-            flag = true;
-            console.log(`Combination N:${counter} (${i} + ${j} = ${sum})`);
+            if (sum == magicNumber) {
+                flag = true;
+                console.log(`Combination N:${counter} (${i} + ${j} = ${sum})`);
+                break;
+            }
+        }
+
+        if (flag == true) {
             break;
         }
+
     }
 
-    if(flag == true){
-        break;
+    if (flag == false) {
+        console.log(`${counter} combinations - neither equals ${magicNumber}`);
     }
-    
-}
-
-if(flag == false){
-    console.log(`${counter} combinations - neither equals ${magicNumber}`);
-}
-
 
 }
 sumOfTwoNumbers(["1",
-"10",
-"5"]);
+    "10",
+    "5"]);
