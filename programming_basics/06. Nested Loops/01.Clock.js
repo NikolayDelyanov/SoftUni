d6b2802ec747e221db4a4810618eff842795d5f6
NikolayDelyanov <125092041+NikolayDelyanov@users.noreply.github.com>
2023-05-01 13:36:37 +0300

    01.Clock

--- programming_basics/06. Nested Loops/01.Clock.js
+++ programming_basics/06. Nested Loops/01.Clock.js
@@ -1,9 +1,24 @@
-function clock(input){
+function clock(input) {
 
-    for(let h = 0; h <= 23; h++){
-       for(let m = 0; m <= 59; m++){
-        console.log(`${h}:${m}`);
-       }
+    for (let h = 0; h <= 23; h++) {
+        for (let m = 0; m <= 59; m++) {
+            let hours = "";
+            let minutes = "";
+
+            if(h < 10){
+                hours = `0${h}`;
+            } else{
+                hours = `${h}`;
+            }
+
+            if(m < 10){
+                minutes = `0${m}`;
+            } else{
+                minutes = `${m}`;
+            }
+
+            console.log(`${hours}:${minutes}`);
+        }
     }
 
 
