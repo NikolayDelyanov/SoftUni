7f0b7c0564b0c0ffdb544d3fd3caf475978141d3
NikolayDelyanov <125092041+NikolayDelyanov@users.noreply.github.com>
2023-05-03 17:15:13 +0300

    06.Building

--- programming_basics/06. Nested Loops/06.Building.js
+++ programming_basics/06. Nested Loops/06.Building.js
@@ -1,27 +1,27 @@
-function building(input){
+function building(input) {
 
-let floorCount = Number(input[0]);
-let roomCount = Number(input[1]);
+    let floorCount = Number(input[0]);
+    let roomCount = Number(input[1]);
 
-for(let floorNumber = floorCount; floorNumber >= 1; floorNumber--){
-    let roomType = "";
+    for (let floorNumber = floorCount; floorNumber >= 1; floorNumber--) {
+        let roomType = "";
 
-    if(floorNumber === floorCount){
-        roomType = "L";
-    } else if(floorNumber % 2 == 0){
-        roomType = "O";
-    } else {
-        roomType = "A";
+        if (floorNumber === floorCount) {
+            roomType = "L";
+        } else if (floorNumber % 2 == 0) {
+            roomType = "O";
+        } else {
+            roomType = "A";
+        }
+
+        let floor = "";
+        for (let roomNumber = 0; roomNumber < roomCount; roomNumber++) {
+            floor = floor + `${roomType}${floorNumber}${roomNumber} `;
+        }
+
+        console.log(floor);
     }
-
-    let floor = "";
-    for(let roomNumber = 0; roomNumber < roomCount; roomNumber++){
-        floor = floor + `${roomType}${floorNumber}${roomNumber} `;
-    }
-
-    console.log(floor);
-}
-
+    
 }
 building(["6",
-"4"]);
+    "4"]);
