0b5bcdbe574b170e2833a749ad9ce4637539b605
NikolayDelyanov <125092041+NikolayDelyanov@users.noreply.github.com>
2023-05-06 00:12:14 +0300

    06.Cinema tickets

--- programming_basics/06. Nested Loops/Exercise/06.Cinema_tickets.js
+++ programming_basics/06. Nested Loops/Exercise/06.Cinema_tickets.js
@@ -1,72 +1,72 @@
-function cinemaTickets(input){
+function cinemaTickets(input) {
 
-let index = 0;
-let command = input[index];
-let studentCounter = 0;
-let standardCounter = 0;
-let kidsCounter = 0;
-let totalTicketsCounter = 0;
+    let index = 0;
+    let command = input[index];
+    let studentCounter = 0;
+    let standardCounter = 0;
+    let kidsCounter = 0;
+    let totalTicketsCounter = 0;
 
-while(command !== 'Finish'){
-    let name = command;
-    index++;
-    let freeSpaces = Number(input[index]);
-    index++;
+    while (command !== 'Finish') {
+        let name = command;
+        index++;
+        let freeSpaces = Number(input[index]);
+        index++;
 
-    let ticketType = input[index];
-    let ticketCounter = 0;
+        let ticketType = input[index];
+        let ticketCounter = 0;
 
-    while(ticketType !== 'End'){
-        ticketCounter++;
+        while (ticketType !== 'End') {
+            ticketCounter++;
 
-        switch(ticketType){
-            case 'standard':
-                standardCounter++;
+            switch (ticketType) {
+                case 'standard':
+                    standardCounter++;
+                    break;
+                case 'student':
+                    studentCounter++;
+                    break;
+                case 'kid':
+                    kidsCounter++;
+                    break;
+            }
+
+            if (ticketCounter >= freeSpaces) {
                 break;
-            case 'student':
-                studentCounter++;
-                break;
-            case 'kid':
-                kidsCounter++;
-                break;
+            }
+
+            ticketType = input[++index];
         }
 
-        if(ticketCounter >= freeSpaces){
-            break;
-        }
+        totalTicketsCounter += ticketCounter;
+        let resultSingleFilm = ticketCounter / freeSpaces * 100;
 
-        ticketType = input[++index];
+        console.log(`${name} - ${resultSingleFilm.toFixed(2)}% full.`);
+        command = input[++index];
     }
 
-    totalTicketsCounter += ticketCounter;
-    let resultSingleFilm = ticketCounter / freeSpaces * 100;
-
-    console.log(`${name} - ${resultSingleFilm.toFixed(2)}% full.`);
-    command = input[++index];
-}
-
-console.log(`Total tickets: ${totalTicketsCounter}`);
-console.log(`${(studentCounter / totalTicketsCounter * 100).toFixed(2)}% student tickets.`);
-console.log(`${(standardCounter / totalTicketsCounter * 100).toFixed(2)}% standard tickets.`);
-console.log(`${(kidsCounter / totalTicketsCounter * 100).toFixed(2)}% kids tickets.`);
+    console.log(`Total tickets: ${totalTicketsCounter}`);
+    console.log(`${(studentCounter / totalTicketsCounter * 100).toFixed(2)}% student tickets.`);
+    console.log(`${(standardCounter / totalTicketsCounter * 100).toFixed(2)}% standard tickets.`);
+    console.log(`${(kidsCounter / totalTicketsCounter * 100).toFixed(2)}% kids tickets.`);
 
 
 }
 cinemaTickets(["Taxi",
-"10",
- "standard", 
- "kid",
-  "student",
-   "student",
+    "10",
     "standard",
-     "standard", 
-     "End",
-      "Scary Movie",
-       "6",
-        "student",
-         "student", 
-         "student",
-          "student",
-           "student",
-            "student",
-             "Finish"]);
+    "kid",
+    "student",
+    "student",
+    "standard",
+    "standard",
+    "End",
+    "Scary Movie",
+    "6",
+    "student",
+    "student",
+    "student",
+    "student",
+    "student",
+    "student",
+    "Finish"]);
