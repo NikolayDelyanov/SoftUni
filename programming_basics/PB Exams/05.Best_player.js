3166b2cf7233734f436e36e6f72ec36d8f6c2b74
NikolayDelyanov <125092041+NikolayDelyanov@users.noreply.github.com>
2023-05-07 01:38:43 +0300

    05.Best player

--- programming_basics/PB Exams/05.Best_player.js
+++ programming_basics/PB Exams/05.Best_player.js
@@ -2,10 +2,33 @@ function bestPlayer(input){
 
 let index = 0;
 let command = input[index];
+let bestPlayer = '';
+let totalGoals = 0;
 
 while(command !== 'END'){
-    let goals = Number(input[index++]);
-    
+    index++;
+    let goals = Number(input[index]);
+
+    if(goals > totalGoals){
+        totalGoals = goals;
+        bestPlayer = command;
+        console.log(`${bestPlayer} is the best player!`);
+    }
+
+    if(goals >= 3){
+        console.log(`He has scored ${totalGoals} goals and made a hat-trick !!!`);
+    } else {
+        console.log(`He has scored ${totalGoals} goals.`);
+    }
+
+    if(goals >= 10){
+        break;
+    }
+
+
+    index++;
+    command = input[index];
+
 }
 
 
