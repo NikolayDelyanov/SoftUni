4444d46b005c2e28a3fddd69d37cb5ecdde259d7
NikolayDelyanov <125092041+NikolayDelyanov@users.noreply.github.com>
2023-05-07 00:45:18 +0300

    05.Best player

---
+++ programming_basics/PB Exams/05.Best_player.js
@@ -0,0 +1,21 @@
+function bestPlayer(input){
+
+let index = 0;
+let command = input[index];
+
+while(command !== 'END'){
+    let goals = Number(input[index++]);
+    
+}
+
+
+
+}
+bestPlayer(["Neymar",
+    "2",
+    "Ronaldo",
+    "1",
+    "Messi",
+    "3",
+    "END"
+    ]);
