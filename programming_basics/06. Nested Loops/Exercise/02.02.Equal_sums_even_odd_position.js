f7b64dab3d3331ce587b1bb45a786e87f24a2929
NikolayDelyanov <125092041+NikolayDelyanov@users.noreply.github.com>
2023-05-09 16:03:01 +0300

    02.Equal sums

--- programming_basics/06. Nested Loops/Exercise/02.02.Equal_sums_even_odd_position.js
+++ programming_basics/06. Nested Loops/Exercise/02.02.Equal_sums_even_odd_position.js
@@ -12,8 +12,8 @@ for(let i  = firstNum; i <= secondNum; i++){
     let evenSum = 0;
 
     for(let j = 0; j < currentNum.length; j++){
-        
-        let currentDigit = Number(currentNum.charAt(j));
+        let currentDigit = Number(currentNum.charAt(j)); 
+
          if(j % 2 === 0){
             evenSum += currentDigit;
          } else {
