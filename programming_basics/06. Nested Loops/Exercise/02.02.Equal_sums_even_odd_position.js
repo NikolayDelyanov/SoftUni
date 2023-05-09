19495e4e62e586eb5b2b91c83536e2a6cbf50661
NikolayDelyanov <125092041+NikolayDelyanov@users.noreply.github.com>
2023-05-09 16:03:45 +0300

    02.Equal Sums

--- programming_basics/06. Nested Loops/Exercise/02.02.Equal_sums_even_odd_position.js
+++ programming_basics/06. Nested Loops/Exercise/02.02.Equal_sums_even_odd_position.js
@@ -1,33 +1,33 @@
-function equalSums(input){
+function equalSums(input) {
 
-let firstNum = Number(input[0]);
-let secondNum = Number(input[1]);
+    let firstNum = Number(input[0]);
+    let secondNum = Number(input[1]);
 
-let printLine = '';
+    let printLine = '';
 
-for(let i  = firstNum; i <= secondNum; i++){
+    for (let i = firstNum; i <= secondNum; i++) {
 
-    let currentNum = "" + i;
-    let oddSum = 0;
-    let evenSum = 0;
+        let currentNum = "" + i;
+        let oddSum = 0;
+        let evenSum = 0;
 
-    for(let j = 0; j < currentNum.length; j++){
-        let currentDigit = Number(currentNum.charAt(j)); 
+        for (let j = 0; j < currentNum.length; j++) {
+            let currentDigit = Number(currentNum.charAt(j));
 
-         if(j % 2 === 0){
-            evenSum += currentDigit;
-         } else {
-            oddSum += currentDigit;
-         }
+            if (j % 2 === 0) {
+                evenSum += currentDigit;
+            } else {
+                oddSum += currentDigit;
+            }
+        }
+
+        if (oddSum === evenSum) {
+            printLine += ` ${i}`;
+        }
     }
 
-    if(oddSum === evenSum){
-        printLine += ` ${i}`;
-    }
-}
-
-console.log(printLine);
+    console.log(printLine);
 
 }
 equalSums(["123456",
-"124000"]);
+    "124000"]);
