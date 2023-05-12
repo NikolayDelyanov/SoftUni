653f756befbe5e4197cc035ef38a5ce974ffd79b
NikolayDelyanov <125092041+NikolayDelyanov@users.noreply.github.com>
2023-05-12 22:59:10 +0300

    02.Easter party

---
+++ programming_basics/PB Exams/PB Exam2/02.Easter_party.js
@@ -0,0 +1,29 @@
+function easterParty(input){
+
+let numberOfGuests = Number(input[0]);
+let priceForOnePerson = Number(input[1]);
+let budget = Number(input[2]);
+
+
+if(numberOfGuests >= 10 && numberOfGuests <= 15){
+     priceForOnePerson -= priceForOnePerson * 0.15;
+} else if(numberOfGuests >= 15 && numberOfGuests <= 20){
+     priceForOnePerson -= priceForOnePerson * 0.20;
+} else if(numberOfGuests > 20){
+     priceForOnePerson -= priceForOnePerson * 0.25;
+}
+
+let priceForCake = budget * (10 / 100);
+let totalSum = (numberOfGuests * priceForOnePerson) + priceForCake;
+
+if(budget >= totalSum){
+    console.log(`It is party time! ${(budget - totalSum).toFixed(2)} leva left.`);
+} else{
+    console.log(`No party! ${(totalSum - budget).toFixed(2)} leva needed.`);
+}
+
+
+}
+easterParty(["8",
+"25",
+"340"]);
