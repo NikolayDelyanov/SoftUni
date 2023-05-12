9b4ee98bd0ba8f4a3fdf9ebf9256f151036cda50
NikolayDelyanov <125092041+NikolayDelyanov@users.noreply.github.com>
2023-05-13 01:39:46 +0300

    05.Easter eggs

--- programming_basics/PB Exams/PB Exam2/05.Easter_eggs.js
+++ programming_basics/PB Exams/PB Exam2/05.Easter_eggs.js
@@ -1,55 +1,54 @@
-function easterEggs(input){
+function easterEggs(input) {
 
-let numPaintedEggs = Number(input[0]);
+    let numPaintedEggs = Number(input[0]);
 
-let red = 0;
-let orange = 0;
-let blue = 0;
-let green = 0;
-let maxCounter = Number.MIN_SAFE_INTEGER;
-let maxEggs = "";
+    let red = 0;
+    let orange = 0;
+    let blue = 0;
+    let green = 0;
+    let maxCounter = Number.MIN_SAFE_INTEGER;
+    let maxEggs = "";
 
-for (let index = 1; index <= input.length; index++) {
-    let paint = input[index];
+    for (let index = 1; index <= input.length; index++) {
+        let paint = input[index];
+
+        if (paint === "red") {
+            red++;
+        } else if (paint === "orange") {
+            orange++;
+        } else if (paint === "blue") {
+            blue++;
+        } else if (paint === "green") {
+            green++;
+        }
+
+        if (maxCounter < red) {
+            maxCounter = red;
+            maxEggs = "red";
+        } else if (maxCounter < orange) {
+            maxCounter = orange;
+            maxEggs = "orange";
+        } else if (maxCounter < blue) {
+            maxCounter = blue;
+            maxEggs = "blue";
+        } else if (maxCounter < green) {
+            maxCounter = green;
+            maxEggs = "green";
+        }
+    }
+
+    console.log(`Red eggs: ${red}`);
+    console.log(`Orange eggs: ${orange}`);
+    console.log(`Blue eggs: ${blue}`);
+    console.log(`Green eggs: ${green}`);
+    console.log(`Max eggs: ${maxCounter} -> ${maxEggs}`);
     
-    if(paint === "red"){
-        red++;
-    } else if(paint === "orange"){
-        orange++;
-    } else if(paint === "blue"){
-        blue++;
-    } else if(paint === "green"){
-        green++;
-    }
-
-    if(maxCounter < red){
-        maxCounter = red;
-        maxEggs = "red";
-    } else if(maxCounter < orange){
-        maxCounter = orange;
-        maxEggs = "orange";
-    } else if(maxCounter < blue){
-        maxCounter = blue;
-        maxEggs = "blue";
-    } else if(maxCounter < green){
-        maxCounter = green;
-        maxEggs = "green";
-    }
-}
-
-console.log(`Red eggs: ${red}`);
-console.log(`Orange eggs: ${orange}`);
-console.log(`Blue eggs: ${blue}`);
-console.log(`Green eggs: ${green}`);
-console.log(`Max eggs: ${maxCounter} -> ${maxEggs}`);
-
-
 }
 easterEggs(["7",
-"orange",
-"blue",
-"green",
-"green",
-"blue",
-"red",
-"green"]);
+    "orange",
+    "blue",
+    "green",
+    "green",
+    "blue",
+    "red",
+    "green"]);
