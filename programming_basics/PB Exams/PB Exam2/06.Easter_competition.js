442f7bfa438aa4b1b118269d7e0ab47d6273e558
NikolayDelyanov <125092041+NikolayDelyanov@users.noreply.github.com>
2023-05-13 01:36:55 +0300

    06.Easter competition

---
+++ programming_basics/PB Exams/PB Exam2/06.Easter_competition.js
@@ -0,0 +1,36 @@
+function easterFinal(input){
+
+let numOfEasterBreads = Number(input[0]);
+
+
+ 
+for(let i = 1; i < input.length; i++){
+
+let index = 1;
+let command = input[index];
+let totalPoints = 0;
+let maxPoints = 0;
+
+while(command !== 'Stop'){
+    let name = command;
+    let points = Number(input[index++]);
+    totalPoints += points;
+
+
+}
+
+}
+
+}
+easterFinal(["2",
+"Chef Angelov",
+"9",
+"9",
+"9",
+"Stop",
+"Chef Rowe",
+"10",
+"10",
+"10",
+"10",
+"Stop"]);
