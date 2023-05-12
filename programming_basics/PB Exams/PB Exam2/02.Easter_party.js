874da4c76ffd40c88828a6123cb2658e8cecd2b4
NikolayDelyanov <125092041+NikolayDelyanov@users.noreply.github.com>
2023-05-13 01:43:10 +0300

    02.Easter party

--- programming_basics/PB Exams/PB Exam2/02.Easter_party.js
+++ programming_basics/PB Exams/PB Exam2/02.Easter_party.js
@@ -1,27 +1,27 @@
-function easterParty(input){
+function easterParty(input) {
 
-let numberOfGuests = Number(input[0]);
-let priceForOnePerson = Number(input[1]);
-let budget = Number(input[2]);
+    let numberOfGuests = Number(input[0]);
+    let priceForOnePerson = Number(input[1]);
+    let budget = Number(input[2]);
 
-if(numberOfGuests >= 10 && numberOfGuests <= 15){
-     priceForOnePerson -= priceForOnePerson * 0.15;
-} else if(numberOfGuests >= 15 && numberOfGuests <= 20){
-     priceForOnePerson -= priceForOnePerson * 0.20;
-} else if(numberOfGuests > 20){
-     priceForOnePerson -= priceForOnePerson * 0.25;
-}
+    if (numberOfGuests >= 10 && numberOfGuests <= 15) {
+        priceForOnePerson -= priceForOnePerson * 0.15;
+    } else if (numberOfGuests >= 15 && numberOfGuests <= 20) {
+        priceForOnePerson -= priceForOnePerson * 0.20;
+    } else if (numberOfGuests > 20) {
+        priceForOnePerson -= priceForOnePerson * 0.25;
+    }
 
-let priceForCake = budget * (10 / 100);
-let totalSum = (numberOfGuests * priceForOnePerson) + priceForCake;
+    let priceForCake = budget * (10 / 100);
+    let totalSum = (numberOfGuests * priceForOnePerson) + priceForCake;
 
-if(budget >= totalSum){
-    console.log(`It is party time! ${(budget - totalSum).toFixed(2)} leva left.`);
-} else{
-    console.log(`No party! ${(totalSum - budget).toFixed(2)} leva needed.`);
-}
+    if (budget >= totalSum) {
+        console.log(`It is party time! ${(budget - totalSum).toFixed(2)} leva left.`);
+    } else {
+        console.log(`No party! ${(totalSum - budget).toFixed(2)} leva needed.`);
+    }
 
 }
 easterParty(["8",
-"25",
-"340"]);
+    "25",
+    "340"]);
