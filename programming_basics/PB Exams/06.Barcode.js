2e4734d7d3484605960122dc7000ba9b73a9d4e8
NikolayDelyanov <125092041+NikolayDelyanov@users.noreply.github.com>
2023-05-12 22:13:30 +0300

    06.Barcode generator

--- programming_basics/PB Exams/06.Barcode.js
+++ programming_basics/PB Exams/06.Barcode.js
@@ -1,28 +1,28 @@
-function Barcode(input){
+function Barcode(input) {
 
     let startNum = Number(input[0]);
     let endNum = Number(input[1]);
     let result = "";
-    
-    for(let barcode = startNum; barcode <= endNum; barcode++){
+
+    for (let barcode = startNum; barcode <= endNum; barcode++) {
         let barcodeString = barcode.toString();
         let evenNumbers = false;
-    
-        for(let i = 0; i < barcodeString.length; i++){
-            if(parseInt(barcodeString[i]) % 2 === 0){
+
+        for (let i = 0; i < barcodeString.length; i++) {
+            if (parseInt(barcodeString[i]) % 2 === 0) {
                 evenNumbers = true;
                 break;
             }
         }
 
-        if(!evenNumbers){
-            result += barcode + " ";  
+        if (!evenNumbers) {
+            result += barcode + " ";
         }
-    
-    
+
+
     }
-    
+
     console.log(result);
-    
-    }
-    Barcode(["2345","6789"]);
+
+}
+Barcode(["2345", "6789"]);
