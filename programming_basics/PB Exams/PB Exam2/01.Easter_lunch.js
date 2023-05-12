feaa3c3b260ad80decea3e5a51358f52717378d1
NikolayDelyanov <125092041+NikolayDelyanov@users.noreply.github.com>
2023-05-13 01:44:10 +0300

    01.Easter lunch

--- programming_basics/PB Exams/PB Exam2/01.Easter_lunch.js
+++ programming_basics/PB Exams/PB Exam2/01.Easter_lunch.js
@@ -1,23 +1,23 @@
 //Start exam 22:25
 
-function easterLunch(input){
+function easterLunch(input) {
 
-let numEasterBread = Number(input[0]);
-let numEggShells = Number(input[1]);
-let kilosOfCookies = Number(input[2]);
+    let numEasterBread = Number(input[0]);
+    let numEggShells = Number(input[1]);
+    let kilosOfCookies = Number(input[2]);
 
-let oneShell = 12;
+    let oneShell = 12;
 
-let priceForBread = numEasterBread * 3.20;
-let priceForEggs = numEggShells * 4.35;
-let priceForCookies = kilosOfCookies * 5.40; 
-let priceForPaint = numEggShells * oneShell * 0.15;
+    let priceForBread = numEasterBread * 3.20;
+    let priceForEggs = numEggShells * 4.35;
+    let priceForCookies = kilosOfCookies * 5.40;
+    let priceForPaint = numEggShells * oneShell * 0.15;
 
-let totalPrice = priceForBread + priceForEggs + priceForCookies + priceForPaint;
+    let totalPrice = priceForBread + priceForEggs + priceForCookies + priceForPaint;
 
-console.log(totalPrice.toFixed(2));
+    console.log(totalPrice.toFixed(2));
 
 }
 easterLunch(["3",
-"2",
-"3"]);
+    "2",
+    "3"]);
