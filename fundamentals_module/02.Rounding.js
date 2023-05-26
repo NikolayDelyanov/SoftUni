6d7d6853f1250f5c04ad3bd056c2b1078e3d5a71
NikolayDelyanov <125092041+NikolayDelyanov@users.noreply.github.com>
2023-05-26 17:55:09 +0300

    02.Rounding

--- fundamentals_module/02.Rounding.js
+++ fundamentals_module/02.Rounding.js
@@ -1,10 +1,10 @@
-function rounding(number, precision){
+function rounding(number, precision) {
 
-if(precision > 15){
-    precision = 15;
+    if (precision > 15) {
+        precision = 15;
+    }
+
+    console.log(parseFloat(number.toFixed(precision)));
+
 }
-
-console.log(parseFloat(number.toFixed(precision)));
-
-}
-rounding(3.1415926535897932384626433832795,2);
+rounding(3.1415926535897932384626433832795, 2);
