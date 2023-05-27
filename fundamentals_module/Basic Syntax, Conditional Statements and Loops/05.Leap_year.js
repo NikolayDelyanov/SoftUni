4f754cb432cab98390bc11fd2087b44cde52a773
NikolayDelyanov <125092041+NikolayDelyanov@users.noreply.github.com>
2023-05-28 00:49:44 +0300

    05.Leap year

--- fundamentals_module/Basic Syntax, Conditional Statements and Loops/05.Leap_year.js
+++ fundamentals_module/Basic Syntax, Conditional Statements and Loops/05.Leap_year.js
@@ -1,10 +1,10 @@
-function leapYear(year){
+function leapYear(year) {
 
-if((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0){
-    console.log('yes');
-} else {
-    console.log('no');
-}
+    if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+        console.log('yes');
+    } else {
+        console.log('no');
+    }
 
 }
 leapYear(2024);
