522a60505aa89bedae8ad84000a65dad79316fb0
NikolayDelyanov <125092041+NikolayDelyanov@users.noreply.github.com>
2023-05-28 01:00:05 +0300

    06.Print and sum

--- fundamentals_module/Basic Syntax, Conditional Statements and Loops/06.Print_and_sum.js
+++ fundamentals_module/Basic Syntax, Conditional Statements and Loops/06.Print_and_sum.js
@@ -1,18 +1,18 @@
-function printAndSum(start , end){
+function printAndSum(start, end) {
 
-let buffNumber = "";
-let sum = 0;
+    let buffNumber = "";
+    let sum = 0;
 
-for(let i = start; i <= end; i++){
-    buffNumber += i + " ";
-    sum += i;
+    for (let i = start; i <= end; i++) {
+        buffNumber += i + " ";
+        sum += i;
+    }
+
+    console.log(buffNumber);
+    console.log(`Sum: ${sum}`);
+
+
 }
-
-console.log(buffNumber);
-console.log(`Sum: ${sum}`);
-
-
-}
-printAndSum(5,10);
+printAndSum(5, 10);
 console.log("-------------");
-printAndSum(45,63);
+printAndSum(45, 63);
