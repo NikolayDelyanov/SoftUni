b4f7e78d0bdcaa7fa9402977078e63d8571be928
NikolayDelyanov <125092041+NikolayDelyanov@users.noreply.github.com>
2023-06-26 17:04:07 +0300

    06.Even and Odd Subtractions

---
+++ fundamentals_module/01.Basic Syntax, Conditional Statements and Loops/Arrays/06.Even_and_Odd_Subtraction.js
@@ -0,0 +1,22 @@
+function evenAndOddSubtractions(array){
+
+let evenSum = 0;
+let oddSum = 0;
+
+for (let index = 0; index < array.length; index++) {
+     array[index] = Number(array[index]);
+}
+
+for(let num of array){
+    if(num % 2 == 0){
+        evenSum += num;
+    } else {
+        oddSum += num;
+    }
+}
+
+
+console.log(evenSum - oddSum);
+
+}
+evenAndOddSubtractions([1,2,3,4,5,6]);
