70efc367afec9cad99f11358560e82ca540cc25e
NikolayDelyanov <125092041+NikolayDelyanov@users.noreply.github.com>
2023-06-26 16:46:06 +0300

    05.Sum even numbers

--- fundamentals_module/Basic Syntax, Conditional Statements and Loops/Arrays/05.Sum_even_numbers.js
+++ fundamentals_module/Basic Syntax, Conditional Statements and Loops/Arrays/05.Sum_even_numbers.js
@@ -1,17 +1,17 @@
-function sumEvenNumbers(array){
+function sumEvenNumbers(array) {
 
-let sum = 0;
-for (let index = 0; index < array.length; index++) {
-     array[index] = Number(array[index]);
-}
-
-for(let num of array){
-    if(num  % 2 == 0){
-        sum += num;
+    let sum = 0;
+    for (let index = 0; index < array.length; index++) {
+        array[index] = Number(array[index]);
     }
-}
 
-console.log(sum);
+    for (let num of array) {
+        if (num % 2 == 0) {
+            sum += num;
+        }
+    }
+
+    console.log(sum);
 
 }
-sumEvenNumbers(['1','2','3','4','5','6']);
+sumEvenNumbers(['1', '2', '3', '4', '5', '6']);
