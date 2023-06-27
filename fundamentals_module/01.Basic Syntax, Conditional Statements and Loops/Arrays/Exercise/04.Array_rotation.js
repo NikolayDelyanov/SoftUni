d52cde48dec9da1f7eb1ef99a9720906fd5be329
NikolayDelyanov <125092041+NikolayDelyanov@users.noreply.github.com>
2023-06-27 11:57:18 +0300

    04.Array rotation

---
+++ fundamentals_module/01.Basic Syntax, Conditional Statements and Loops/Arrays/Exercise/04.Array_rotation.js
@@ -0,0 +1,19 @@
+function arrayRotation(array, rotationCount){
+
+let buffArr = [];
+
+for (let i = 0; i < rotationCount; i++) {
+    let currElement = array[0]; 
+    array.shift();
+    array.push(currElement);
+}
+
+console.log(array.join(" "));
+
+}
+arrayRotation([51, 47, 32, 61, 21], 2 );
+console.log('-------------------');
+arrayRotation([32, 21, 61, 1], 4 );
+console.log('-------------------');
+arrayRotation([2, 4, 15, 31], 5 );
+console.log('-------------------');
