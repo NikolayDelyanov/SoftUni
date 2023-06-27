4b571079644b59f1a9670a9dd3bbf9b9fc4ccf71
NikolayDelyanov <125092041+NikolayDelyanov@users.noreply.github.com>
2023-06-27 09:43:03 +0300

    08.Condense array for number

---
+++ fundamentals_module/01.Basic Syntax, Conditional Statements and Loops/Arrays/08.Condense_array_for_number.js
@@ -0,0 +1,19 @@
+
+function condenseArray(arr){
+    while (arr.length > 1) {
+    
+        let newArr = [];
+    
+        for (let i = 0; i < arr.length - 1; i++) {
+
+            let currEl = arr[i] + arr[i+1];
+            
+            newArr.push(currEl);
+        }
+        arr = newArr;
+    }
+
+    console.log(arr[0]);
+
+}
+condenseArray([2,10,3]);
