dfddc06aa31e903da8168c85831b7fdf939c84ab
NikolayDelyanov <125092041+NikolayDelyanov@users.noreply.github.com>
2023-06-27 11:06:37 +0300

    02.Common elements

--- fundamentals_module/01.Basic Syntax, Conditional Statements and Loops/Arrays/Exercise/02.Common_elements.js
+++ fundamentals_module/01.Basic Syntax, Conditional Statements and Loops/Arrays/Exercise/02.Common_elements.js
@@ -1,17 +1,17 @@
-function commonElements(firstArr,secondArr){
+function commonElements(firstArr, secondArr) {
 
     for (let i = 0; i < firstArr.length; i++) {
         for (let y = 0; y < secondArr.length; y++) {
-            if(firstArr[i] === secondArr[y]){
+            if (firstArr[i] === secondArr[y]) {
                 console.log(firstArr[i]);
             }
-            
+
         }
 
-}
+    }
 }
 commonElements(['Hey', 'hello', 2, 4, 'Peter', 'e'],
-['Petar', 10, 'hey', 4, 'hello', '2']);
+    ['Petar', 10, 'hey', 4, 'hello', '2']);
 console.log("><><><><><><><><><><><><><><><><>");
 commonElements(['S', 'o', 'f', 't', 'U', 'n', 'i', ' '],
-['s', 'o', 'c', 'i', 'a', 'l']);
+    ['s', 'o', 'c', 'i', 'a', 'l']);
