bd24e2845885b453a2e17dc150021cfdb1e7639b
NikolayDelyanov <125092041+NikolayDelyanov@users.noreply.github.com>
2023-06-27 10:31:29 +0300

    01.Add and Subtract

--- fundamentals_module/01.Basic Syntax, Conditional Statements and Loops/Arrays/Exercise/01.Add_And_Subtract.js
+++ fundamentals_module/01.Basic Syntax, Conditional Statements and Loops/Arrays/Exercise/01.Add_And_Subtract.js
@@ -1,30 +1,28 @@
-function addAndSubtract(array){
+function addAndSubtract(array) {
 
-let modifiedArray = [];
-let originalSum = array.reduce((a,b) => a+b,0);
-let modifiedSum = 0;
+    let modifiedArray = [];
+    let originalSum = array.reduce((a, b) => a + b, 0);
+    let modifiedSum = 0;
 
-for (let index = 0; index < array.length; index++) {
-    let number = array[index];
-    
-    let modifiedNum = 0;
+    for (let index = 0; index < array.length; index++) {
+        let number = array[index];
 
-    if(number % 2 === 0){
-         modifiedNum = number + index;
-        modifiedArray.push(modifiedNum);
-        modifiedSum += modifiedNum;
-    }else {
-        modifiedNum = number - index;
-        modifiedArray.push(modifiedNum);
-        modifiedSum += modifiedNum;    
-    }  
+        let modifiedNum = 0;
 
-}
+        if (number % 2 === 0) {
+            modifiedNum = number + index;
+            modifiedArray.push(modifiedNum);
+            modifiedSum += modifiedNum;
+        } else {
+            modifiedNum = number - index;
+            modifiedArray.push(modifiedNum);
+            modifiedSum += modifiedNum;
+        }
+    }
 
-console.log(modifiedArray);
-console.log(originalSum);
-console.log(modifiedSum);
-
+    console.log(modifiedArray);
+    console.log(originalSum);
+    console.log(modifiedSum);
 
 }
 addAndSubtract([5, 15, 23, 56, 35]);
