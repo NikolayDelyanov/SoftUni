f4db36a0abe2e6104263f1b435f072f28cb0114d
NikolayDelyanov <125092041+NikolayDelyanov@users.noreply.github.com>
2023-06-30 13:37:08 +0300

    02.Much Power

--- fundamentals_module/Functions/02.Much_power.js
+++ fundamentals_module/Functions/02.Much_power.js
@@ -1,12 +1,6 @@
 function muchPower(number, power){
 
-    let result = 1;
-
-for(let i = 1; i <= power; i++){
-    result *= number;
-}
-
-console.log(result);
+console.log(Math.pow(number,power));
 
 }
 muchPower(2,8);
