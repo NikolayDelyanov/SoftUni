320ddbe8d9602fe32290f8e3a32d3717b97dffa0
NikolayDelyanov <125092041+NikolayDelyanov@users.noreply.github.com>
2023-06-30 15:16:02 +0300

    04.Integer and float

---
+++ fundamentals_module/Data Types And Variables/04.Integer_And_Float.js
@@ -0,0 +1,14 @@
+function integerAndFloat(firstNum, secondNum, thirdNum) {
+
+    let sum = firstNum + secondNum + thirdNum;
+
+    if (sum % 1 === 0) {
+        sum += ' - Integer';
+    } else {
+        sum += ' - Float';
+    }
+
+    console.log(sum);
+
+}
+integerAndFloat(100, 200, 303);
