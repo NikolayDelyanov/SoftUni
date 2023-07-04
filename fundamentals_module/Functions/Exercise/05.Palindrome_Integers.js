95dd66f50c95e0eacc2541b984646e638ce8ec7a
NikolayDelyanov <125092041+NikolayDelyanov@users.noreply.github.com>
2023-07-04 11:03:03 +0300

    05.Palindrome Integers

--- fundamentals_module/Functions/Exercise/05.Palindrome_Integers.js
+++ fundamentals_module/Functions/Exercise/05.Palindrome_Integers.js
@@ -1,14 +1,14 @@
-function palindrome(arr){
+function palindrome(arr) {
 
-for (let i = 0; i < arr.length; i++) {
-    let numToString = String(arr[i]);
-    let buff = "";
-    for(let j = numToString.length - 1; j >= 0; j--){
-        buff += numToString[j];
-    }    
+    for (let i = 0; i < arr.length; i++) {
+        let numToString = String(arr[i]);
+        let buff = "";
+        for (let j = numToString.length - 1; j >= 0; j--) {
+            buff += numToString[j];
+        }
 
-    console.log(buff === numToString);
+        console.log(buff === numToString);
+    }
+
 }
-
-}
-palindrome([123,323,421,121]);
+palindrome([123, 323, 421, 121]);
