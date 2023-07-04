692758e76457207d36d97fe33751fed7da54c2d2
NikolayDelyanov <125092041+NikolayDelyanov@users.noreply.github.com>
2023-07-04 15:25:09 +0300

    08. Perfect number

--- fundamentals_module/Functions/Exercise/08.Perfect_number.js
+++ fundamentals_module/Functions/Exercise/08.Perfect_number.js
@@ -1,23 +1,19 @@
-function perfectNumber(number){
-    
+function perfectNumber(number) {
+
     let temp = 0;
-   for(let i = 1; i <= number/2; i++)
-     {
-         if(number % i === 0)
-          {
+    for (let i = 1; i <= number / 2; i++) {
+        if (number % i === 0) {
             temp += i;
-          }
-     }
-   
-     if(temp === number && temp !== 0)
-        {
-       console.log("We have a perfect number!");
-        } 
-     else
-        {
-       console.log("It's not so perfect.");
-        }   
- }
+        }
+    }
+
+    if (temp === number && temp !== 0) {
+        console.log("We have a perfect number!");
+    }
+    else {
+        console.log("It's not so perfect.");
+    }
+}
 perfectNumber(6);
 console.log('-----------');
 perfectNumber(28);
