e966493192556f168470a976d7859668c9b8e7e0
NikolayDelyanov <125092041+NikolayDelyanov@users.noreply.github.com>
2023-07-04 13:17:17 +0300

    06. Password validator

---
+++ fundamentals_module/Functions/Exercise/06.Password_validator.js
@@ -0,0 +1,35 @@
+function passwordValidator(pass){
+    let digits = 0;
+    let isValidLength = true;
+
+    if(pass.length < 6 || pass.length > 10){
+        console.log("Password must be between 6 and 10 characters");
+        isValidLength = false;
+    }
+
+    let isOnlyLettersAndDigigts = /^[A-Za-z0-9]*$/.test(pass);
+
+    for(let i = 0; i < pass.length; i++){
+        console.log();
+        if(/\d/.test(pass[i])) {
+            digits++
+        } 
+    }
+
+    if (digits >= 2 && isOnlyLettersAndDigigts && isValidLength) {
+        console.log('Password is valid');
+    } else {
+         if (isOnlyLettersAndDigigts === false) {
+            console.log("Password must consist only of letters and digits");
+         } 
+        
+         if (digits < 2){
+            console.log("Password must have at least 2 digits");
+         }
+     }
+}
+passwordValidator('MyPass123');
+console.log('------------');
+passwordValidator('logIn');
+console.log('------------');
+passwordValidator('Pa$s$s');
