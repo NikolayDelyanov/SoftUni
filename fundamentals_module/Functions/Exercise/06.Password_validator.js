cb0aef5f8be179c64401f4be48a49ccce203d85e
NikolayDelyanov <125092041+NikolayDelyanov@users.noreply.github.com>
2023-07-04 13:34:16 +0300

    06.Password validator

--- fundamentals_module/Functions/Exercise/06.Password_validator.js
+++ fundamentals_module/Functions/Exercise/06.Password_validator.js
@@ -1,32 +1,24 @@
 function passwordValidator(pass) {
 
-    let digits = 0;
-    let isValidLength = true;
-
-    if (pass.length < 6 || pass.length > 10) {
-        console.log("Password must be between 6 and 10 characters");
-        isValidLength = false;
-    }
-
     let isOnlyLettersAndDigigts = /^[A-Za-z0-9]*$/.test(pass);
 
-    for (let i = 0; i < pass.length; i++) {
-        console.log();
-        if (/\d/.test(pass[i])) {
-            digits++
-        }
-    }
+    let digits = /^[A-Za-z]*[0-9]{2,}/.test(pass);
+    let isValidLength = pass.length >= 6 && pass.length <= 10;
 
-    if (/^[A-Za-z]*[0-9]{2,}/.test(pass)) {
+    if (isOnlyLettersAndDigigts && digits && isValidLength) {
         console.log('Password is valid');
     } else {
 
-        if (isOnlyLettersAndDigigts === false) {
+        if (!isOnlyLettersAndDigigts) {
             console.log("Password must consist only of letters and digits");
         }
 
-        if (digits < 2) {
+        if (!digits) {
             console.log("Password must have at least 2 digits");
+        }
+
+        if (!isValidLength) {
+            console.log("Password must be between 6 and 10 characters");
         }
     }
 }
