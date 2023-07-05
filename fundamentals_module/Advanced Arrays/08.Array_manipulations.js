013420a7f3e6047434a42e287ac75b68fba50bf0
NikolayDelyanov <125092041+NikolayDelyanov@users.noreply.github.com>
2023-07-05 16:40:44 +0300

    08. Array manipulations

--- fundamentals_module/Advanced Arrays/08.Array_manipulations.js
+++ fundamentals_module/Advanced Arrays/08.Array_manipulations.js
@@ -1,47 +1,46 @@
-function manipulations(commands){
+function manipulations(commands) {
 
-let arr = commands.shift().split(' ').map(Number);
+    let arr = commands.shift().split(' ').map(Number);
 
-for (let i = 0; i < commands.length; i++) {
-    let [command , firstNum , secondNum] = commands[i].split(' ');
+    for (let i = 0; i < commands.length; i++) {
+        let [command, firstNum, secondNum] = commands[i].split(' ');
 
-    firstNum = Number(firstNum);
-    secondNum = Number(secondNum);
+        firstNum = Number(firstNum);
+        secondNum = Number(secondNum);
 
-    switch(command){
-        case 'Add':
-            add(firstNum,secondNum);
-            break;
-        case 'Remove':
-            remove(firstNum,secondNum);
-            break;
-        case 'RemoveAt':
-            removeat(firstNum,secondNum);
-            break;
-        case 'Insert':
-            insert(firstNum,secondNum);
-            break;
+        switch (command) {
+            case 'Add':
+                add(firstNum, secondNum);
+                break;
+            case 'Remove':
+                remove(firstNum, secondNum);
+                break;
+            case 'RemoveAt':
+                removeat(firstNum, secondNum);
+                break;
+            case 'Insert':
+                insert(firstNum, secondNum);
+                break;
+        }
     }
 
-   
-}
+    function add(el) {
+        arr.push(el);
+    }
 
-function add(el){
-    arr.push(el);
-}
+    function remove(num) {
+        arr = arr.filter(el => el !== num);
+    }
 
-function remove(num){
-    arr = arr.filter(el => el !== num);
-}
+    function removeat(index) {
+        arr.splice(index, 1);
+    }
 
-function removeat(index){
-    arr.splice(index,1);
-}
+    function insert(num, index) {
+        arr.splice(index, 0, num);
+    }
 
-function insert(num, index){
-    arr.splice(index,0,num);
+    console.log(arr.join(' '));
+    
 }
-
-console.log(arr.join(' '));
-}
-manipulations(['6 12 2 65 6 42','Add 8','Remove 12','RemoveAt 3','Insert 6 2']);
+manipulations(['6 12 2 65 6 42', 'Add 8', 'Remove 12', 'RemoveAt 3', 'Insert 6 2']);
