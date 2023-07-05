d9080c53cace55e1dc7c5b0e215d151e30094a0d
NikolayDelyanov <125092041+NikolayDelyanov@users.noreply.github.com>
2023-07-05 13:29:51 +0300

    05. Process odd numbers

---
+++ fundamentals_module/Advanced Arrays/05.Process_odd_numbers.js
@@ -0,0 +1,14 @@
+function processOddNums(arr){
+
+let filtered  = arr.filter((x,i)=>i % 2 == 1);
+
+let doubled = filtered.map(x => x * 2);
+
+let result = doubled.reverse();
+
+console.log(result.join(' '));
+
+}
+processOddNums([10, 15, 20, 25]);
+
+processOddNums([3, 0, 10, 4, 7, 3]);
