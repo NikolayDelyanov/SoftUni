4050c5e2a311bc57313538938105939d2cff73c0
NikolayDelyanov <125092041+NikolayDelyanov@users.noreply.github.com>
2023-07-05 12:23:17 +0300

    03. First and last k

---
+++ fundamentals_module/Advanced Arrays/03.First_and_last_k.js
@@ -0,0 +1,12 @@
+function firstLastK(arr){
+
+    let k = arr.shift();
+
+    let firstK = arr.slice(0,k);
+    let secondK = arr.slice(-k);
+
+    console.log(firstK.join(' '));
+    console.log(secondK.join(' '));
+
+}
+firstLastK([2, 7, 8, 9]);
