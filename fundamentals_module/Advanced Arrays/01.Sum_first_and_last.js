4f1e7aac299b1069cdd676be7a6bcd30c7fc5292
NikolayDelyanov <125092041+NikolayDelyanov@users.noreply.github.com>
2023-07-05 10:48:58 +0300

    01. Sum first and last

---
+++ fundamentals_module/Advanced Arrays/01.Sum_first_and_last.js
@@ -0,0 +1,10 @@
+function sum(input){
+
+    let firstSum = input.shift();
+    let secondSum = input.pop();
+
+    console.log(Number(firstSum) + Number(secondSum));
+
+
+}
+sum(['20', '30', '40'] );
