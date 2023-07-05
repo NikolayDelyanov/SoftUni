a30c3acc44878bae77e0f024b634e265a72a2c87
NikolayDelyanov <125092041+NikolayDelyanov@users.noreply.github.com>
2023-07-05 10:34:58 +0300

    07. Max sequence element

---
+++ fundamentals_module/Arrays/Exercise/07.Max_sequence_of_equal_element.js
@@ -0,0 +1,33 @@
+function maxSequence(arr) {
+
+    let longestSequence = [];
+    let leftMostIndex = 0;
+
+    for (let i = 0; i < arr.length; i++) {
+
+        let currentSequence = [arr[i]];
+
+        for (let j = i + 1; j < arr.length; j++) {
+
+            let nextEl = arr[j];
+
+            if (nextEl !== arr[i]) {
+                break;
+            }
+
+            currentSequence.push(nextEl)
+        }
+
+        if (currentSequence.length > longestSequence.length) {
+
+            longestSequence = [...currentSequence];
+        }
+    }
+
+    console.log(longestSequence.join(' '));
+}
+console.log('---------------');
+maxSequence([2, 1, 1, 2, 3, 3, 2, 2, 2, 1]);
+console.log('---------------');
+maxSequence([1, 1, 1, 2, 3, 1, 3, 3]);
+console.log('---------------');
