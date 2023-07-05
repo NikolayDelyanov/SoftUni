55c86ad697e0de93bd471c769b9f485441874a19
NikolayDelyanov <125092041+NikolayDelyanov@users.noreply.github.com>
2023-07-05 10:30:26 +0300

    01.Sum first and last array element

---
+++ fundamentals_module/Arrays/01.Sum_first_and_last_arrayElement.js
@@ -0,0 +1,32 @@
+function sumFirstAndLastArrayElements(arr){
+
+    let firstElement = arr[0];
+    let lastIndex = arr.length - 1;
+    let lastElements = arr[lastIndex];
+    console.log(firstElement + lastElements);
+    
+    
+    }
+    let myArr = [11, 58, 69];
+    sumFirstAndLastArrayElements(myArr);
+    sumFirstAndLastArrayElements([10, 17, 22, 33]);
+    
+    
+    let num = [1,10,3];
+    
+    while (num.length>1) {
+        let counter = num.length;
+    
+        let curArr = [];
+    
+        for (let i = 0; i < num.length; i++) {
+            
+            if (num[i+1] > num.length-1) {
+                continue;
+            }
+            curArr[i] = num[i]+num[i+1];
+             
+                   
+        }
+        num = curArr;
+    }
