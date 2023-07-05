e3564703dde5baa93ec748af53c0e8f2f5bd5706
NikolayDelyanov <125092041+NikolayDelyanov@users.noreply.github.com>
2023-07-05 13:53:22 +0300

    06. Smalest Nums

--- fundamentals_module/Advanced Arrays/06.Smallest_two_numbers.js
+++ fundamentals_module/Advanced Arrays/06.Smallest_two_numbers.js
@@ -1,6 +1,4 @@
-function smallestNums(arr){
-
-console.log(arr.sort((a,b) => a - b).slice(0,2).join(' '));
-
+function smallestNums(arr) {
+    console.log(arr.sort((a, b) => a - b).slice(0, 2).join(' '));
 }
 smallestNums([30, 15, 50, 5]);
