3e22375b56d2d996de60408a44e4f603c056167f
NikolayDelyanov <125092041+NikolayDelyanov@users.noreply.github.com>
2023-07-18 11:07:08 +0300

    Rotate

--- fundamentals_module/Arrays/Exercise/More Exercise/03.Rotate.js
+++ fundamentals_module/Arrays/Exercise/More Exercise/03.Rotate.js
@@ -2,6 +2,7 @@ function rotate(arr) {
 
     let rotations = +arr[arr.length - 1];
     arr.pop();
+    
     for (let i = 1; i <= rotations; i++) {
         let current = arr.pop();
         arr.unshift(current);
