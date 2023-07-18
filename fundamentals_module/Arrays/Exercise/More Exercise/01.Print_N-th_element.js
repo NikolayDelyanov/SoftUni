17eebc50df7917bee1ddac86665266eeb3f8c4d2
NikolayDelyanov <125092041+NikolayDelyanov@users.noreply.github.com>
2023-07-18 10:24:29 +0300

    Print N-th element

---
+++ fundamentals_module/Arrays/Exercise/More Exercise/01.Print_N-th_element.js
@@ -0,0 +1,12 @@
+function printNthElement(arr) {
+    
+    let lastNum = arr.pop();
+    let resultArr = [];
+    for (let index in arr) {
+        if (index % lastNum === 0) {
+            resultArr.push(arr[index]);
+        }
+    }
+    console.log(resultArr.join(' '));
+}
+printNthElement(['5', '20', '31', '4', '20', '2']);
