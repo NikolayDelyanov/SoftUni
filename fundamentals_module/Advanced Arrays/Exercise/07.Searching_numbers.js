5a8defbe11ab8f995b81015aabe54bae85507fe8
NikolayDelyanov <125092041+NikolayDelyanov@users.noreply.github.com>
2023-07-18 10:03:27 +0300

    Search for a number

---
+++ fundamentals_module/Advanced Arrays/Exercise/07.Searching_numbers.js
@@ -0,0 +1,19 @@
+function search(arr1,arr2){
+
+    let count = 0;
+    let elements = arr2[2];
+
+    let arr = arr1.slice(0, arr2[0]);
+    arr = arr.slice(arr2[1], arr.length);
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === elements) {
+            count++;
+        }
+    }
+
+    console.log(`Number ${elements} occurs ${count} times.`);
+
+
+}
+search([5, 2, 3, 4, 1, 6] , [5, 2, 3]);
