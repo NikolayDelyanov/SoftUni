bcaccf465211de663321321d5c7afd5bbdcac8a9
NikolayDelyanov <125092041+NikolayDelyanov@users.noreply.github.com>
2023-07-20 11:09:02 +0300

    Magic matrices

---
+++ fundamentals_module/Arrays/Exercise/More Exercise/06.Magic_matrices.js
@@ -0,0 +1,28 @@
+function solve(matrix) {
+
+    const reduce = (prev, curr) => prev + curr;
+    let sum = matrix[0].reduce(reduce);
+    for (let row = 0; row < matrix.length; row++) {
+        let rowSum = matrix[row].reduce(reduce);
+        if (rowSum !== sum) {
+            return false;
+        }
+        if (row === 0) {
+            for (let col = 0; col < matrix[row].length; col++) {
+                let columSum = 0;
+                for (let i = 0; i < matrix.length; i++) {
+                    columSum += matrix[col][i];
+                }
+                if (columSum !== sum) {
+                    return false;
+                }
+            }
+        }
+    }
+    return true;
+}
+magicMatrices([4, 5, 6],
+
+    [6, 5, 4],
+
+    [5, 5, 5]);
